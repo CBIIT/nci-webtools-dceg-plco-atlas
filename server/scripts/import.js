608a85a39de3e1e27aac689fcad0fd661f260ff1
Brian Park <park-brian@users.noreply.github.com>
2019-06-27 21:25:09 -0400

    implement import function

--- server/scripts/import.js
+++ server/scripts/import.js
@@ -26,9 +26,11 @@ if (fs.existsSync(databaseFilePath)) {
     process.exit(1);
 }
 
-// create database
+// initialize database
 const db = new sqlite.Database(databaseFilePath);
 
+// used to calculate duration
+const startTime = new Date();
 
 db.serialize(() => {
 
@@ -47,38 +49,31 @@ db.serialize(() => {
         "I" REAL
     )`);
 
+    db.exec('begin transaction');
+    const placeholders = Array(12).fill('?').join();
+    const stmt = db.prepare(`INSERT INTO meta VALUES (${placeholders})`);
     const reader = readline.createInterface({
         input: fs.createReadStream(metaFilePath)
     });
+    let count = 0;
 
     reader.on('line', line => {
-        console.log(line.trim().split(/\s+/));
+        const values = line.trim().split(/\s+/).map(e => e === 'NA' ? null : e);
+        stmt.run(values);
+
+        // show progress message every 10000 rows
+        if (++count % 10000 === 0)
+            console.log(`PROGRESS: Inserted ${count} rows (${(new Date() - startTime) / 1000} s)`);
     });
 
+    reader.on('close', () => {
+        // stmt.finalize();
+        db.exec('commit');
+        console.log(`ALMOST DONE: Inserted ${count} rows in ${(new Date() - startTime) / 1000} s. Please wait while DB is indexed.`)
+        db.exec(`
+            create index idx_meta_bp on meta(BP);
+            create index idx_meta_p on meta(P);
+        `);
+        db.close(_ => console.log(`DONE: Created database in ${(new Date() - startTime) / 1000} s`));
+    });
 });
-
-db.close();
-
-const a = ` CHR          BP            SNP  A1  A2   N           P        P(R)      OR   OR(R)       Q       I
-1       10177        1:10177   A  AC   3      0.9755      0.8071  1.0033  1.0433  0.0976   57.03
-1       10352        1:10352   T  TA   3      0.2832      0.5357  0.8938  0.9188  0.2012   37.63
-1       11008        1:11008   C   G   3      0.9723      0.9723  0.9945  0.9945  0.8376    0.00
-1       11012        1:11012   C   G   3      0.9723      0.9723  0.9945  0.9945  0.8376    0.00
-1       13110        1:13110   G   A   2      0.9577      0.9577  1.0140  1.0140  0.4764    0.00
-1       13116        1:13116   T   G   3     0.00431     0.00431  0.6741  0.6741  0.7313    0.00
-1       13118        1:13118   A   G   3     0.00431     0.00431  0.6741  0.6741  0.7313    0.00
-1       13273        1:13273   G   C   3      0.4425      0.4425  0.8896  0.8896  0.9087    0.00
-1       14464        1:14464   A   T   3      0.8463      0.8463  1.0275  1.0275  0.7856    0.00
-1       14599        1:14599   T   A   3      0.7915      0.7915  0.9645  0.9645  0.6629    0.00
-1       14604        1:14604   A   G   3      0.7915      0.7915  0.9645  0.9645  0.6629    0.00
-1       14930        1:14930   A   G   3       0.276       0.276  0.8939  0.8939  0.9755    0.00
-1       15211        1:15211   T   G   3      0.7993      0.7993  0.9723  0.9723  0.4326    0.00
-1       15274        1:15274   A   G   3      0.3412      0.3412  1.1092  1.1092  0.6524    0.00
-1       15820        1:15820   G   T   3      0.5955      0.5955  0.9407  0.9407  0.5131    0.00
-1       15903        1:15903   G  GC   3      0.4746      0.4746  0.9296  0.9296  0.5576    0.00
-1       30923        1:30923   G   T   3      0.3013      0.3013  0.8244  0.8244  0.8075    0.00
-1       47159        1:47159   T   C   1          NA          NA      NA      NA      NA      NA
-1       49298        1:49298   T   C   3       0.136       0.136  0.8106  0.8106  0.4114    0.00
-1       49554        1:49554   A   G   3      0.3379      0.3379  1.1976  1.1976  0.5179    0.00
-1       51479        1:51479   T   A   3      0.9494      0.9494  0.9923  0.9923  0.6478    0.00`
-
