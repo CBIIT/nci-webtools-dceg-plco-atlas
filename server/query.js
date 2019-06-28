8d6e0c1949b08df2a4b7c34d28eec5e4d9376251
Brian Park <park-brian@users.noreply.github.com>
2019-06-27 21:43:24 -0400

    update parameters

--- server/query.js
+++ server/query.js
@@ -1,22 +1,21 @@
 const sqlite = require('sqlite3').verbose();
 
-function query(databaseFilePath, minBP, maxBP, minP, maxP) {
+function query(databaseFilePath, bpMin, bpMax, pMin, pMax) {
     return new Promise((resolve, reject) => {
         const db = new sqlite.Database(databaseFilePath, sqlite.OPEN_READONLY);
         const stmt = db.prepare(`
             SELECT * FROM meta WHERE
-                BP >= $minBP AND
-                BP <= $maxBP AND
-                P >= $minP AND
-                P <= $maxP;
+                BP >= $bpMin AND
+                BP <= $bpMax AND
+                P >= $pMin AND
+                P <= $pMax;
         `);
 
-
         stmt.run({
-            $minBP: minBP,
-            $maxBP: maxBP,
-            $minP: minP,
-            $maxP: maxP
+            $bpMin: bpMin,
+            $bpMax: bpMax,
+            $pMin: pMin,
+            $pMax: pMax
         });
 
         stmt.all((err, rows) => err
