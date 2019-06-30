f1ded575b81b7cdcf6e5104ac53fdf9be75f1eb4
Brian Park <park-brian@users.noreply.github.com>
2019-06-30 19:49:15 -0400

    group bp by 10^6, -log10(p) by 10^-2

--- server/scripts/import.js
+++ server/scripts/import.js
@@ -31,16 +31,21 @@ const db = new sqlite.Database(databaseFilePath);
 // used to calculate duration
 const startTime = new Date();
 
+// ensure the following db statements are run in-order
 db.serialize(() => {
+    const tableName = 'variants';
 
-    db.run(`CREATE TABLE meta (
+    db.run(`CREATE TABLE ${tableName} (
         "CHR" INTEGER,
         "BP" INTEGER,
+        "BP_GROUP" INTEGER, -- BP to a precision of 10^6
         "SNP" TEXT,
         "A1" TEXT,
         "A2" TEXT,
         "N" INTEGER,
         "P" REAL,
+        "NLOG_P" REAL, -- negative log10(P)
+        "NLOG_P_GROUP" REAL,  -- negative log10(P) to a precision of 10^-2
         "P_R" REAL,
         "OR" REAL,
         "OR_R" REAL,
@@ -50,23 +55,48 @@ db.serialize(() => {
 
     // set up transaction and prepare insertion statement
     db.exec('begin transaction');
-    const placeholders = Array(12).fill('?').join();
-    const stmt = db.prepare(`INSERT INTO meta VALUES (${placeholders})`);
+    const stmt = db.prepare(`INSERT INTO ${tableName} VALUES (
+        $CHR,
+        $BP,
+        $BP_GROUP,
+        $SNP,
+        $A1,
+        $A2,
+        $N,
+        $P,
+        $NLOG_P,
+        $NLOG_P_GROUP,
+        $P_R,
+        $OR,
+        $OR_R,
+        $Q,
+        $I
+    )`);
 
     // stream the input file line by line
     const reader = readline.createInterface({
         input: fs.createReadStream(inputFilePath)
     });
     let count = 0;
+    let bpGroupSize = Math.pow(10, 6);  // group base pairs by millionths
+    let nlogpGroupSize = Math.pow(10, -2); // group -log(p) by hundredths
 
     // insert each line into the database
     reader.on('line', line => {
+        // skip first line
+        if (count++ == 0) return;
+
         // remove any spaces, and ensure 'NA' values are parsed as null
         const values = line.trim().split(/\s+/).map(e => e === 'NA' ? null : e);
-        stmt.run(values);
+        const [$CHR, $BP, $SNP, $A1, $A2, $N, $P, $P_R, $OR, $OR_R, $Q, $I] = values;
+        const params = {$CHR, $BP, $SNP, $A1, $A2, $N, $P, $P_R, $OR, $OR_R, $Q, $I}
+        params.$NLOG_P = $P ? -Math.log10($P) : null;
+        params.$BP_GROUP = Math.floor($BP / bpGroupSize) * bpGroupSize;
+        params.$NLOG_P_GROUP = Math.floor(params.$NLOG_P / nlogpGroupSize) * nlogpGroupSize;
 
+        stmt.run(params);
         // show progress message every 10000 rows
-        if (++count % 10000 === 0)
+        if (count % 10000 === 0)
             console.log(`PROGRESS: Inserted ${count} rows (${(new Date() - startTime) / 1000} s)`);
     });
 
@@ -76,8 +106,8 @@ db.serialize(() => {
         db.exec('commit');
         console.log(`ALMOST DONE: Inserted ${count} rows in ${(new Date() - startTime) / 1000} s. Please wait while DB is indexed.`)
         db.exec(`
-            create index idx_meta_bp on meta(BP);
-            create index idx_meta_p on meta(P);
+            create index idx_bp on ${tableName}(BP);
+            create index idx_nlog_p on ${tableName}(NLOG_P);
         `);
         db.close(_ => console.log(`DONE: Created database in ${(new Date() - startTime) / 1000} s`));
     });
