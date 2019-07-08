78dfa1e6ab73a16888c51c59021de719aeb1f309
Brian Park <brian.park@nih.gov>
2019-07-08 18:07:47 -0400

    update query functions

--- server/query.js
+++ server/query.js
@@ -1,21 +1,46 @@
 const sqlite = require('sqlite3').verbose();
+const variantTable = 'variant';
+const rangeTable = 'variant_range';
+const summaryTable = 'variant_summary';
 
-function query(databaseFilePath, bpMin, bpMax, pMin, pMax) {
+function getRanges(filepath) {
     return new Promise((resolve, reject) => {
-        const db = new sqlite.Database(databaseFilePath, sqlite.OPEN_READONLY);
-        const stmt = db.prepare(`
-            SELECT * FROM meta WHERE
+        const db = new sqlite.Database(filepath, sqlite.OPEN_READONLY);
+        const sql = `SELECT * FROM ${rangeTable}`;
+        db.all(sql, [], (err, rows) => err
+            ? reject(err)
+            : resolve(rows));
+    });
+}
+
+function getSummary(filepath) {
+    return new Promise((resolve, reject) => {
+        const db = new sqlite.Database(filepath, sqlite.OPEN_READONLY);
+        let sql = `SELECT * FROM ${summaryTable} WHERE NLOG_P2 >= 3`;
+        db.all(sql, [], (err, rows) => err
+            ? reject(err)
+            : resolve(rows));
+    });
+}
+
+function getVariants(filepath, {chr, bpMin, bpMax, nlogpMin, nlogpMax}) {
+    return new Promise((resolve, reject) => {
+        const db = new sqlite.Database(filepath, sqlite.OPEN_READONLY);
+        let stmt = db.prepare(`
+            SELECT * FROM ${variantTable} WHERE
+                CHR = $chr AND
                 BP >= $bpMin AND
                 BP <= $bpMax AND
-                P >= $pMin AND
-                P <= $pMax;
-        `);
+                NLOG_P >= $nlogpMin AND
+                NLOG_P <= $nlogpMax
+            ORDER BY CHR, BP`);
 
         stmt.run({
+            $chr: chr,
             $bpMin: bpMin,
             $bpMax: bpMax,
-            $pMin: pMin,
-            $pMax: pMax
+            $nlogpMin: nlogpMin,
+            $nlogpMax: nlogpMax,
         });
 
         stmt.all((err, rows) => err
@@ -24,4 +49,4 @@ function query(databaseFilePath, bpMin, bpMax, pMin, pMax) {
     });
 }
 
-module.exports = query;
+module.exports = {getRanges, getSummary, getVariants};
