cc286cc25383ad3d50aee6e5ae0dd17a35c53357
Brian Park <brian.park@nih.gov>
2019-07-08 16:13:46 -0400

    add comments

--- server/server.js
+++ server/server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const query = require('./query');
-const config = require('./config.json');
+const { databases, port } = require('./config.json');
+const { query, getSummary, getMetadata } = require('./query');
 const app = express();
 
+// serve static files from the client/build folder
 app.use(express.static('client/build'));
+
+// add cors headers to the response (allow cross-origin requests)
 app.use(cors());
 
-app.get('/query', async (req, res) => {
-    const { database, bpMin, bpMax, pMin, pMax } = req.query;
-    const db = config.databases.find(e => e.name === database);
-    if (!db) return;
+// todo: check connectivity to database
+app.get('/ping', (req, res) => res.json(true));
 
-    const results = await query(db.filepath, bpMin, bpMax, pMin, pMax);
-    res.json(results);
+// retrieves metadata for a particular database (BP/P ranges, # variants, etc)
+app.get('/metadata', async ({query}, res) => {
+    const db = databases.find(e => e.name === query.database);
+    res.json(await getMetadata(db.filepath));
 });
 
-app.listen(config.port || 9000);
+// retrieves all variant groups for all chroms. at the lowest granularity (in MBases)
+app.get('/summary', async ({query}, res) => {
+    const db = databases.find(e => e.name === query.database);
+    res.json(await getSummary(db.filepath));
+});
+
+// retrieves all variants at the specified granularity (using 1000 kb, 100 kb, or individual)
+app.get('/query', async ({query}, res) => {
+    const db = databases.find(e => e.name === query.database);
+    res.json(await query(db.filepath, query));
+});
+
+app.listen(port, _ => console.log(`Application is running on port: ${port}`));
