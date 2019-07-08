f902f6c9844cc7ba879e7a5571ff297cd83ff185
Brian Park <brian.park@nih.gov>
2019-07-08 18:01:36 -0400

    create materialized views for summary/range data

--- server/scripts/import.js
+++ server/scripts/import.js
@@ -5,14 +5,14 @@ const sqlite = require('sqlite3').verbose();
 const headers = ['CHR','BP','SNP','A1','A2','N','P','P(R)','OR','OR(R)','Q','I'];
 
 // retrieve arguments
-const args = process.argv.slice(2);
-if (args.length === 0 || args[0] === '-h') {
+const argv = process.argv.slice(2);
+if (argv.length !== 2 || argv.includes('-h')) {
     console.log(`USAGE: node import.js input.meta output.db`)
     process.exit(0);
 }
 
 // parse arguments and set defaults
-let [ inputFilePath, databaseFilePath ] = args;
+let [ inputFilePath, databaseFilePath ] = argv;
 databaseFilePath = databaseFilePath || 'output.db';
 
 // input file should exist
@@ -36,57 +36,87 @@ const duration = _ => ((new Date() - startTime) / 1000).toPrecision(4);
 
 // ensure the following db statements are executed in the order they are created
 db.serialize(() => {
-    const tableName = 'variant';
+    const variantTable = 'variant';
+    const summaryTable = 'variant_summary';
+    const rangeTable = 'variant_range';
 
-    db.run(`CREATE TABLE ${tableName} (
-        "CHR" INTEGER,
-        "BP" INTEGER,
-        "BP_1000KB" INTEGER, -- BP to a precision of 10^6 (1 megabase)
-        "BP_100KB" INTEGER, -- BP to a precision of 10^5 (100 kilobases)
-        "SNP" TEXT,
-        "A1" TEXT,
-        "A2" TEXT,
-        "N" INTEGER,
-        "P" REAL,
-        "NLOG_P" REAL, -- negative log10(P)
-        "NLOG_P2" REAL, -- negative log10(P) to a precision of 10^-2
-        "P_R" REAL,
-        "OR" REAL,
-        "OR_R" REAL,
-        "Q" REAL,
-        "I" REAL
-    )`);
+    // variant table stores all variant information
+    db.run(`
+        CREATE TABLE ${variantTable}
+        (
+            "CHR"       INTEGER,
+            "BP"        INTEGER,
+            "BP_1000KB" INTEGER, -- BP floored to the nearest multiple of 10^6 (1000 kilobases)
+            "BP_100KB"  INTEGER, -- BP floored to the nearest multiple of 10^5 (100 kilobases)
+            "SNP"       TEXT,
+            "A1"        TEXT,
+            "A2"        TEXT,
+            "N"         INTEGER,
+            "P"         REAL,
+            "NLOG_P"    REAL, -- negative log10(P)
+            "NLOG_P2"   REAL, -- negative log10(P) floored to the nearest multiple of 10^-2
+            "P_R"       REAL,
+            "OR"        REAL,
+            "OR_R"      REAL,
+            "Q"         REAL,
+            "I"         REAL
+        );
+    `);
+
+    // stores a materialized summary view, which contains all variants at the lowest granularity
+    db.run(`
+        CREATE TABLE ${summaryTable}
+        (
+            "CHR"       INTEGER,
+            "BP_1000KB" INTEGER, -- BP floored to the nearest multiple of 10^6 (1000 kilobases)
+            "NLOG_P2"   REAL, -- negative log10(P) floored to the nearest multiple of 10^-2
+        );
+    `);
+
+    // stores a materialized range view for each chromosome's min/max bp and nlog_p values
+    db.run(`
+        CREATE TABLE ${rangeTable}
+        (
+            "CHR"        INTEGER,
+            "MIN_BP"     INTEGER,
+            "MAX_BP"     INTEGER,
+            "MIN_NLOG_P" REAL,
+            "MAX_NLOG_P" REAL
+        );
+    `);
 
     // set up transaction and prepare insertion statement
     db.exec('begin transaction');
-    const stmt = db.prepare(`INSERT INTO ${tableName} VALUES (
-        $CHR,
-        $BP,
-        $BP_1000KB,
-        $BP_100KB,
-        $SNP,
-        $A1,
-        $A2,
-        $N,
-        $P,
-        $NLOG_P,
-        $NLOG_P2,
-        $P_R,
-        $OR,
-        $OR_R,
-        $Q,
-        $I
-    )`);
+    const stmt = db.prepare(`
+        INSERT INTO ${variantTable} VALUES (
+            $CHR,
+            $BP,
+            $BP_1000KB,
+            $BP_100KB,
+            $SNP,
+            $A1,
+            $A2,
+            $N,
+            $P,
+            $NLOG_P,
+            $NLOG_P2,
+            $P_R,
+            $OR,
+            $OR_R,
+            $Q,
+            $I
+        )
+    `);
 
     // stream the input file line by line
     const reader = readline.createInterface({
         input: fs.createReadStream(inputFilePath)
     });
 
-    // floors a value to the lowest multiple of the size
-    const group = (value, size) => (
+    // floors a value to the lowest multiple of the size given (usually a power of 10)
+    const group = (value, size) => +(
         size * Math.floor(value / size)
-    );
+    ).toPrecision(16);
 
     // insert each line into the database
     let count = 0;
@@ -99,7 +129,7 @@ db.serialize(() => {
             try {
                 return assert.deepEqual(values, headers);
             } catch(e) {
-                console.error(`ERROR: Headers do not match expected values:`, headers)
+                console.error(`ERROR: Headers do not match expected values:`, headers);
                 process.exit(3);
             }
         }
@@ -122,18 +152,49 @@ db.serialize(() => {
             console.log(`[${duration()} s] Inserted ${count} rows`);
     });
 
-    // after reading every line in the file, commit the transaction and create indexes
+    // after reading every line in the file, commit the transaction
     reader.on('close', () => {
         // stmt.finalize(); // no need to finalize statement
         db.exec('commit');
-        console.log(`[${duration()} s] Inserted ${count} rows. DB is being indexed...`)
+
+        // create indexes for columns we will be querying off
+        console.log(`[${duration()} s] Inserted ${count} rows. DB is being indexed...`);
         db.exec(`
-            create index idx_bp on ${tableName}(BP);
-            create index idx_bp_1000kb on ${tableName}(BP_1000KB);
-            create index idx_bp_100kb on ${tableName}(BP_100KB);
-            create index idx_nlog_p on ${tableName}(NLOG_P);
-            create index idx_nlog_p2 on ${tableName}(NLOG_P2);
+            CREATE INDEX idx_${variantTable}_chr       ON ${variantTable}(CHR);
+            CREATE INDEX idx_${variantTable}_bp        ON ${variantTable}(BP);
+            CREATE INDEX idx_${variantTable}_bp_1000kb ON ${variantTable}(BP_1000KB);
+            CREATE INDEX idx_${variantTable}_bp_100kb  ON ${variantTable}(BP_100KB);
+            CREATE INDEX idx_${variantTable}_nlog_p    ON ${variantTable}(NLOG_P);
+            CREATE INDEX idx_${variantTable}_nlog_p2   ON ${variantTable}(NLOG_P2);
         `);
+
+        // store summary table for variants
+        console.log(`[${duration()} s] Storing summary...`);
+        db.exec(`
+            INSERT INTO ${summaryTable} SELECT DISTINCT
+                CHR, BP_1000KB, NLOG_P2
+            FROM ${variantTable}
+            ORDER BY CHR, BP_1000KB, NLOG_P2;
+
+            CREATE INDEX idx_${summaryTable}_chr       ON ${summaryTable}(CHR);
+            CREATE INDEX idx_${summaryTable}_bp_1000kb ON ${summaryTable}(BP_1000KB);
+            CREATE INDEX idx_${summaryTable}_nlog_p2   ON ${summaryTable}(NLOG_P2);
+        `);
+
+        // store min/max position and nlog_p values for each chromosome
+        console.log(`[${duration()} s] Storing ranges...`);
+        db.exec(`
+            INSERT INTO ${rangeTable} SELECT
+                CHR,
+                MIN(BP) AS MIN_BP,
+                MAX(BP) AS MAX_BP,
+                MIN(NLOG_P) AS MIN_NLOG_P,
+                MAX(NLOG_P) AS MAX_NLOG_P
+            FROM ${variantTable}
+            GROUP BY CHR
+            ORDER BY CHR, BP;
+        `);
+
         db.close(_ => console.log(`[${duration()} s] Created database`));
     });
 });
