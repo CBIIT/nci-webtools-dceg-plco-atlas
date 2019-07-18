9fba78599802546d7b587270ee61be4624ca7f71
Brian Park <brian.park@nih.gov>
2019-07-18 14:47:51 -0400

    reformat queries

--- server/query.js
+++ server/query.js
@@ -16,7 +16,9 @@ function getRanges(filepath) {
 function getSummary(filepath) {
     return new Promise((resolve, reject) => {
         const db = new sqlite.Database(filepath, sqlite.OPEN_READONLY);
-        let sql = `SELECT * FROM ${summaryTable} WHERE NLOG_P2 >= 3
+        let sql = `SELECT distinct CHR, BP_ABS_1000KB, NLOG_P2
+            FROM ${summaryTable}
+            WHERE NLOG_P2 >= 3
             ORDER BY CHR ASC, BP_ABS_1000KB ASC, NLOG_P2 DESC`;
         db.all(sql, [], (err, rows) => err
             ? reject(err)
@@ -29,12 +31,11 @@ function getVariants(filepath, {chr, bpMin, bpMax, nlogpMin, nlogpMax}) {
         const db = new sqlite.Database(filepath, sqlite.OPEN_READONLY);
         let stmt = db.prepare(`
             SELECT * FROM ${variantTable} WHERE
-                CHR = $chr AND
-                BP >= $bpMin AND
-                BP <= $bpMax AND
-                NLOG_P >= $nlogpMin AND
-                NLOG_P <= $nlogpMax
-            ORDER BY CHR, BP`);
+                CHR = $chr
+                AND BP >= $bpMin
+                AND BP <= $bpMax
+                AND NLOG_P >= $nlogpMin
+                AND NLOG_P <= $nlogpMax`);
 
         stmt.run({
             $chr: chr,
