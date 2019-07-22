80f54c27d79b4317fd6ee0f61a7b47a93cc6b182
kvnjng <kvvnjng@gmail.com>
2019-07-22 10:58:45 -0400

    change navbar style & homepage margins

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export function Navbar({ links }) {
   return (
     <div className="bg-primary text-white">
-      <div className="container py-1">
-        {/* <h1 className="h5 m-0">
-          PLCO Aggregation Prototype
-        </h1> */}
-        <Link className="text-white mr-3" to="/">
-          Home
-        </Link>
-        {[]
+      <div className="container">
+        {[{route: '/', title: 'Home', exact: true}]
           .concat(links)
           .sort((a, b) => a.navIndex - b.navIndex)
-          .map(({ route, title }) => (
-            <Link className="text-white ml-3 mr-3" to={route}>
+          .map(({ route, title, exact }) => (
+            <NavLink className="navlinks text-white mr-3 py-1 px-3 d-inline-block" activeClassName="active-navlinks" exact={exact} to={route}>
               {title}
-            </Link>
+            </NavLink>
           ))}
         {/* <pre>{JSON.stringify(links)}</pre> */}
       </div>
