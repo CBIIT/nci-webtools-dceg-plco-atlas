771f20581e1945c6089fe0ee75872103d746bbad
Brian Park <brian.park@nih.gov>
2019-07-23 17:53:51 -0400

    add cache-control headers

--- server/server.js
+++ server/server.js
@@ -14,18 +14,21 @@ app.get('/ping', (req, res) => res.send(true));
 // retrieves metadata for a particular database (BP/P ranges, # variants, etc)
 app.get('/ranges', ({query}, res) => {
     const db = databases.find(e => e.name === query.database);
+    res.header('Cache-Control', 'max-age=300');
     res.send(getRanges(db.filepath));
 });
 
 // retrieves all variant groups for all chroms. at the lowest granularity (in MBases)
 app.get('/summary', ({query}, res) => {
     const db = databases.find(e => e.name === query.database);
-    res.send(getSummary(db.filepath));
+    res.header('Cache-Control', 'max-age=300');
+    res.send(getSummary(db.filepath, query));
 });
 
 // retrieves all variants within the specified range
 app.get('/variants', ({query}, res) => {
     const db = databases.find(e => e.name === query.database);
+    res.header('Cache-Control', 'max-age=300');
     res.send(getVariants(db.filepath, query));
 });
 
