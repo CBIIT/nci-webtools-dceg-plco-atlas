eb0ec888178b67b9b8c832ff7551f80dd98c737f
Brian Park <park-brian@users.noreply.github.com>
2019-08-20 23:53:51 -0400

    make axis lines thinner

--- client/src/services/plots/axis.js
+++ client/src/services/plots/axis.js
@@ -22,15 +22,17 @@ export function axisLeft(canvas, config) {
 
   const context = canvas.getContext('2d');
   context.translate((xOffset || 0) - tickSize - padding, yOffset || 0);
-  context.fillStyle = 'black';
-  context.font = '600 12px ' + font;
+  context.fillStyle = '#444';
+  context.font = '600 10px ' + font;
   context.textAlign = 'right';
   context.textBaseline = 'middle';
-  context.globalAlpha = 1;
+  context.globalAlpha = 0.5;
 
-  context.fillRect(tickSize, 0, 2, scale(tickValues[0]));
+  context.fillRect(tickSize, 0, 1, scale(tickValues[0]));
   yCoords.forEach((yCoord, i) => {
-    context.fillRect(0, yCoord, tickSize, 2);
+    context.globalAlpha = 0.5;
+    context.fillRect(0, yCoord, tickSize, 1);
+    context.globalAlpha = 1;
     context.fillText(tickValues[i], -padding, yCoord + 2);
   });
 
@@ -67,20 +69,22 @@ export function axisBottom(canvas, config) {
 
   const context = canvas.getContext('2d');
   context.translate(xOffset || 0, yOffset || 0);
-  context.fillStyle = 'black';
-  context.font = '600 12px ' + font;
+  context.fillStyle = '#444';
+  context.font = '600 10px ' + font;
   context.textAlign = 'center';
   context.textBaseline = 'top';
-  context.globalAlpha = 1;
+  context.globalAlpha = 0.5;
 
-  context.fillRect(0, 0, scale(max(tickValues)), 2);
+  context.fillRect(0, 0, scale(max(tickValues)), 1);
   coords.forEach((coord, i) => {
-    context.fillRect(coord, 0, 2, tickSize);
+    context.globalAlpha = 0.5;
+    context.fillRect(coord, 0, 1, tickSize);
     const label = tickFormat ? tickFormat(tickValues[i], i) : tickValues[i];
     const labelOffset = config.labelsBetweenTicks
       ? scale(interpolatedTickValues[i])
       : coord;
-    context.fillText(label, labelOffset + 1, padding + tickSize);
+      context.globalAlpha = 1;
+      context.fillText(label, labelOffset + 1, padding + tickSize);
   });
 
   const midpoint = scale(max(tickValues) / 2);
