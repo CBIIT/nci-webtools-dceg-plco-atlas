80ae80ad1a62b9b0d44ad38489bafc8d227ff3bd
kvnjng <kvvnjng@gmail.com>
2019-09-10 14:49:32 -0400

    change secondary navbar style

--- client/src/components/pages/gwas.js
+++ client/src/components/pages/gwas.js
@@ -43,10 +43,10 @@ export function Gwas() {
 
   return (
     <>
-      <Card 
+      <div 
         // className="border-top-0" 
         // style={{borderRadius: '0 0 0.25em 0.25em'}}
-        className="border-top-0 border-left-0 border-right-0" 
+        className="bg-white shadow-sm" 
         style={{borderRadius: '0 0 0 0'}}
         >
         <div className="container">
@@ -56,19 +56,19 @@ export function Gwas() {
                   {/* <NavItem className="mr-2"> */}
                   <NavLink
                     key={pathId}
-                    className="text-secondary ml-1 px-3 py-2 d-inline-block"
+                    className="text-secondary px-3 py-2 d-inline-block"
                     activeClassName="active-secondary-navlinks border-primary border-bottom text-secondary"
                     style={{textDecoration: 'none', fontSize: '11pt', fontWeight: '600'}}
                     exact={true}
                     to={`/gwas/${pathId}`}>
                     {name}
                   </NavLink>
-                  <div className="d-md-none"></div>
+                  <div className="d-md-none w-100"></div>
                 </>
               ))}
             </Nav>
         </div>
-      </Card>
+      </div>
       <div className="container my-4">
         <Route
           exact
