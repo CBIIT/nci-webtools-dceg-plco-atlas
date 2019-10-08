3ea1adc156517353851b0338dcfe702ff415012d
Brian Park <brian.park@nih.gov>
2019-10-08 17:48:06 -0400

    update import script

--- server/scripts/indexes.sql
+++ server/scripts/indexes.sql
@@ -1,12 +1,39 @@
-CREATE INDEX idx_variant_id     ON variant(variant_id);
-CREATE INDEX idx_variant_chr    ON variant(chr);
-CREATE INDEX idx_variant_bp     ON variant(bp);
-CREATE INDEX idx_variant_snp    ON variant(snp);
-CREATE INDEX idx_variant_p      ON variant(p);
-CREATE INDEX idx_variant_nlog_p ON variant(nlog_p);
-CREATE INDEX idx_variant_query  ON variant(chr, bp, nlog_p);
+CREATE INDEX idx_variant_all_id     ON variant_all(variant_id);
+CREATE INDEX idx_variant_all_chr    ON variant_all(chr);
+CREATE INDEX idx_variant_all_bp     ON variant_all(bp);
+CREATE INDEX idx_variant_all_snp    ON variant_all(snp);
+CREATE INDEX idx_variant_all_p      ON variant_all(p);
+CREATE INDEX idx_variant_all_nlog_p ON variant_all(nlog_p);
+CREATE INDEX idx_variant_all_query  ON variant_all(chr, bp, nlog_p);
 
-CREATE INDEX idx_variant_summary_chr           ON variant_summary(chr);
-CREATE INDEX idx_variant_summary_bp_abs_1000kb ON variant_summary(bp_abs_1000kb);
-CREATE INDEX idx_variant_summary_nlog_p2       ON variant_summary(nlog_p2);
-CREATE INDEX idx_variant_summary_query         ON variant_summary(chr, bp_abs_1000kb, nlog_p2);
+CREATE INDEX idx_variant_female_id     ON variant_female(variant_id);
+CREATE INDEX idx_variant_female_chr    ON variant_female(chr);
+CREATE INDEX idx_variant_female_bp     ON variant_female(bp);
+CREATE INDEX idx_variant_female_snp    ON variant_female(snp);
+CREATE INDEX idx_variant_female_p      ON variant_female(p);
+CREATE INDEX idx_variant_female_nlog_p ON variant_female(nlog_p);
+CREATE INDEX idx_variant_female_query  ON variant_female(chr, bp, nlog_p);
+
+CREATE INDEX idx_variant_male_id     ON variant_male(variant_id);
+CREATE INDEX idx_variant_male_chr    ON variant_male(chr);
+CREATE INDEX idx_variant_male_bp     ON variant_male(bp);
+CREATE INDEX idx_variant_male_snp    ON variant_male(snp);
+CREATE INDEX idx_variant_male_p      ON variant_male(p);
+CREATE INDEX idx_variant_male_nlog_p ON variant_male(nlog_p);
+CREATE INDEX idx_variant_male_query  ON variant_male(chr, bp, nlog_p);
+
+
+CREATE INDEX idx_aggregate_all_chr           ON aggregate_all(chr);
+CREATE INDEX idx_aggregate_all_bp_abs_1000kb ON aggregate_all(bp_abs_1000kb);
+CREATE INDEX idx_aggregate_all_nlog_p2       ON aggregate_all(nlog_p2);
+CREATE INDEX idx_aggregate_all_query         ON aggregate_all(chr, bp_abs_1000kb, nlog_p2);
+
+CREATE INDEX idx_aggregate_female_chr           ON aggregate_female(chr);
+CREATE INDEX idx_aggregate_female_bp_abs_1000kb ON aggregate_female(bp_abs_1000kb);
+CREATE INDEX idx_aggregate_female_nlog_p2       ON aggregate_female(nlog_p2);
+CREATE INDEX idx_aggregate_female_query         ON aggregate_female(chr, bp_abs_1000kb, nlog_p2);
+
+CREATE INDEX idx_aggregate_male_chr           ON aggregate_male(chr);
+CREATE INDEX idx_aggregate_male_bp_abs_1000kb ON aggregate_male(bp_abs_1000kb);
+CREATE INDEX idx_aggregate_male_nlog_p2       ON aggregate_male(nlog_p2);
+CREATE INDEX idx_aggregate_male_query         ON aggregate_male(chr, bp_abs_1000kb, nlog_p2);
--- server/scripts/schema.sql
+++ server/scripts/schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE variant_stage
     "i"             REAL
 );
 
-CREATE TABLE variant
+CREATE TABLE variant_all
 (
     "variant_id"    INTEGER PRIMARY KEY,
     "chr"           INTEGER,
@@ -37,9 +37,65 @@ CREATE TABLE variant
     "i"             REAL
 );
 
-CREATE TABLE variant_summary
+CREATE TABLE variant_female
+(
+    "variant_id"    INTEGER PRIMARY KEY,
+    "chr"           INTEGER,
+    "bp"            INTEGER,
+    "snp"           TEXT,
+    "a1"            TEXT,
+    "a2"            TEXT,
+    "n"             INTEGER,
+    "p"             REAL,
+    "nlog_p"        REAL, -- negative log10(P)
+    "p_r"           REAL,
+    "or"            REAL,
+    "or_r"          REAL,
+    "q"             REAL,
+    "i"             REAL
+);
+
+CREATE TABLE variant_male
+(
+    "variant_id"    INTEGER PRIMARY KEY,
+    "chr"           INTEGER,
+    "bp"            INTEGER,
+    "snp"           TEXT,
+    "a1"            TEXT,
+    "a2"            TEXT,
+    "n"             INTEGER,
+    "p"             REAL,
+    "nlog_p"        REAL, -- negative log10(P)
+    "p_r"           REAL,
+    "or"            REAL,
+    "or_r"          REAL,
+    "q"             REAL,
+    "i"             REAL
+);
+
+CREATE TABLE aggregate_all
 (
     "chr"           INTEGER,
     "bp_abs_1000kb" INTEGER, -- Absolute BP floored to the nearest multiple of 10^6 (1000 kilobases)
     "nlog_p2"       REAL -- negative log10(P) floored to the nearest multiple of 10^-2
+);
+
+CREATE TABLE aggregate_female
+(
+    "chr"           INTEGER,
+    "bp_abs_1000kb" INTEGER, -- Absolute BP floored to the nearest multiple of 10^6 (1000 kilobases)
+    "nlog_p2"       REAL -- negative log10(P) floored to the nearest multiple of 10^-2
+);
+
+CREATE TABLE aggregate_male
+(
+    "chr"           INTEGER,
+    "bp_abs_1000kb" INTEGER, -- Absolute BP floored to the nearest multiple of 10^6 (1000 kilobases)
+    "nlog_p2"       REAL -- negative log10(P) floored to the nearest multiple of 10^-2
+);
+
+CREATE TABLE variant_metadata
+(
+    "key"           TEXT PRIMARY KEY,
+    "value"         TEXT
 );
