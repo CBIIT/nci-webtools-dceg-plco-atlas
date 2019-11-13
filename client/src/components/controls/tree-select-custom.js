f0fde8292b715e0c93404e1b4a3412e88bf2ee6b
kvnjng <kvvnjng@gmail.com>
2019-11-13 12:08:10 -0500

    add parent checkbox behavior

--- client/src/components/controls/tree-select-custom.js
+++ client/src/components/controls/tree-select-custom.js
@@ -13,33 +13,33 @@ export function TreeSelectCustom({onChange, data, value}) {
         }
       
         return result;
-      }
+    };
       
     const containsAllVals = (arr, vals) => {
-    let result = true;
-    for (var i = 0; i < vals.length; i++) {
-        if (!containsVal(arr, vals[i].value)) {
-            result = false;
-        } 
-    }
-    return result;
-    }
+        let result = true;
+        for (var i = 0; i < vals.length; i++) {
+            if (!containsVal(arr, vals[i].value)) {
+                result = false;
+            } 
+        }
+        return result;
+    };
     
     const removeVal = (arr, val) => {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].value === val) {
-            arr.splice(i, 1);
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].value === val) {
+                arr.splice(i, 1);
+            }
         }
-    }
-    return arr;
-    }
+        return arr;
+    };
     
     const removeAllVals = (arr, vals) => {
         for (var i = 0; i < vals.length; i++) {
             removeVal(arr, vals[i].value);
         }
         return arr;
-    }
+    };
     
     const getLeafs = (item, node, allLeafs = []) => {
         if(!node.children || node.children.length === 0) {
@@ -48,12 +48,15 @@ export function TreeSelectCustom({onChange, data, value}) {
                 allLeafs.push(node);
             // }
         } else {
+            if (document.getElementsByClassName("parent-checkbox-" + node.value)[0]) {
+                document.getElementsByClassName("parent-checkbox-" + node.value)[0].checked = true;
+            }
             for (var i = 0; i < node.children.length; i++) {
                 allLeafs = getLeafs(item, node.children[i], allLeafs);
             }
         }
         return allLeafs;
-    }
+    };
     
     const getAllLeafs = (item) => {
         let allLeafs = [];
@@ -69,7 +72,7 @@ export function TreeSelectCustom({onChange, data, value}) {
         }
         
         return allLeafs;
-    }
+    };
 
     const toggleHideChildren = name => {
         const className = "children-of-" + name;
@@ -81,19 +84,44 @@ export function TreeSelectCustom({onChange, data, value}) {
             document.getElementsByClassName(className)[0].style.display = "none";
             document.getElementsByClassName("collapse-button-text-" + name)[0].textContent = "show";
         }
-    }
+    };
+
+    const checkParents = () => {
+       console.log(data);
+       // 1 uncheck all sub parents and children when touched parent is unchecked
+       // 2 check parent when all sibling leafs are also checked
+       // 3 uncheck parent when if any of children are unchecked
+    };
 
     const handleSelect = item => {
+        const parentCheckboxClassName = "parent-checkbox-" + item.value;
+        // const leafCheckboxClassName = "leaf-checkbox-" + item.value;
         let values = [...value];
         let newValues = getAllLeafs(item);
         if (containsAllVals(values, newValues)) {
             // remove all leafs if parent is clicked and all leafs were already selected
             values = removeAllVals(values, newValues);
+            if (document.getElementsByClassName(parentCheckboxClassName)[0]) {
+                console.log(document.getElementsByClassName(parentCheckboxClassName));
+                document.getElementsByClassName(parentCheckboxClassName)[0].checked = false;
+            }
+            for (var i = 0; i < newValues.length; i++) {
+                if (document.getElementsByClassName("leaf-checkbox-" + newValues[i].value)[0]) {
+                    document.getElementsByClassName("leaf-checkbox-" + newValues[i].value)[0].checked = false;
+                }
+            }
         } else {
             for (var i = 0; i < newValues.length; i++) {
                 if (!containsVal(values, newValues[i].value)) {
                     // only add if value did not exist before
                     values.push(newValues[i]);
+                    if (document.getElementsByClassName(parentCheckboxClassName)[0]) {
+                        console.log(document.getElementsByClassName(parentCheckboxClassName));
+                        document.getElementsByClassName(parentCheckboxClassName)[0].checked = true;
+                    }
+                    if (document.getElementsByClassName("leaf-checkbox-" + newValues[i].value)[0]) {
+                        document.getElementsByClassName("leaf-checkbox-" + newValues[i].value)[0].checked = true;
+                    }
                 } else {
                     // remove if new selected leaf was already selected
                     if (newValues.length === 1) {
@@ -102,9 +130,14 @@ export function TreeSelectCustom({onChange, data, value}) {
                 }
             }
         }
+        checkParents();
 
         onChange(values);
-    }
+    };
+
+    // const isChecked = (e) => {
+    //     console.log("isChecked", e);
+    // };
 
     const selectTree = data => data.map((item) => {
         if (item.children && item.children.length > 0) {
@@ -112,10 +145,11 @@ export function TreeSelectCustom({onChange, data, value}) {
                 <>
                     <li className="my-1">
                         <input
-                            name="isGoing"
+                            className={"parent-checkbox-" + item.value}
+                            name={"parent-checkbox-" + item.value}
                             type="checkbox"
-                            checked={true}
-                            // onChange={this.handleInputChange} 
+                            // checked={true}
+                            onChange={e => handleSelect(item)} 
                         />
 
                         <span className="text-info ml-1"><i>(<u>parent</u>)</i></span> 
@@ -144,10 +178,11 @@ export function TreeSelectCustom({onChange, data, value}) {
             return(
                 <li>
                     <input
-                        name="isGoing"
+                        className={"leaf-checkbox-" + item.value}
+                        name={"leaf-checkbox-" + item.value}
                         type="checkbox"
-                        // checked={this.state.isGoing}
-                        // onChange={this.handleInputChange} 
+                        // checked={e => isChecked(e)}
+                        onChange={e => handleSelect(item)} 
                     />
 
                     <span className="text-danger ml-1"><i>(<u>leaf</u>)</i></span> 
--- client/src/components/gwas/summary-results.js
+++ client/src/components/gwas/summary-results.js
@@ -350,7 +350,7 @@ export function SummaryResults() {
           </div>
         )}
 
-        <div class="d-md-none p-2"></div>
+        <div className="d-md-none p-2"></div>
       
         <div className={openSidebar ? "col-md-9" : "col-md-12"}>
           <Tabs className="" defaultActiveKey={selectedPlot} onSelect={setSelectedPlot}>
