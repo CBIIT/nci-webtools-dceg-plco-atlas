c12a0c93eb4c074e913b422528de368eff0a8dbd
kvnjng <kvvnjng@gmail.com>
2019-11-15 18:00:44 -0500

    change tooltip implementation for QQ plot and Heatmap

--- client/src/components/plots/qq-plot.js
+++ client/src/components/plots/qq-plot.js
@@ -1,13 +1,14 @@
-// import React, { useState } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
+// import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateSummaryResults } from '../../services/actions';
 import { Link } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
 import {
-  viewportToLocalCoordinates
+  viewportToLocalCoordinates,
+  createElement as h 
 } from '../../services/plots/utils';
-// import ReactCursorPosition from 'react-cursor-position';
+import ReactCursorPosition from 'react-cursor-position';
 
 import Plot from 'react-plotly.js';
 
@@ -23,96 +24,96 @@ export function QQPlot({ onVariantLookup }) {
     sampleSize, 
     popupTooltipData,
     qqplotData,
-    qqplotLayout
+    qqplotLayout,
   } = useSelector(state => state.summaryResults);
 
   // temporary set states
-  // const [hoverTooltipData, setHoverTooltipData] = useState({});
+  const [hoverTooltipData, setHoverTooltipData] = useState({});
 
-  // const setPopupTooltipData = popupTooltipData => {
-  //   dispatch(updateSummaryResults({ popupTooltipData }));
-  // };
+  const setPopupTooltipData = popupTooltipData => {
+    dispatch(updateSummaryResults({ popupTooltipData }));
+  };
 
-  // const [pos, setPos] = useState({
-  //   position: {
-  //     x: 0,
-  //     y: 0
-  //   }
-  // });
+  const [pos, setPos] = useState({
+    position: {
+      x: 0,
+      y: 0
+    }
+  });
 
-  // const [hoverTooltipStyle, setHoverTooltipStyle] = useState({
-  //   top: 0, // computed based on child and parent's height
-  //   left: 0, // computed based on child and parent's width
-  //   display: 'none'
-  // });
+  const [hoverTooltipStyle, setHoverTooltipStyle] = useState({
+    top: 0, // computed based on child and parent's height
+    left: 0, // computed based on child and parent's width
+    display: 'none'
+  });
 
-  // const [popupTooltipStyle, setPopupTooltipStyle] = useState({
-  //   top: 0, // computed based on child and parent's height
-  //   left: 0, // computed based on child and parent's width
-  //   display: 'none'
-  // });
+  const [popupTooltipStyle, setPopupTooltipStyle] = useState({
+    top: 0, // computed based on child and parent's height
+    left: 0, // computed based on child and parent's width
+    display: 'none'
+  });
 
-  // const popupMarkerClick = e => {
-  //   // make sure action occurs on imagemap coord only
-  //   if (e.target && e.target.coords) {
-  //     var variant = e.target.alt.split(',');
-  //     setPopupTooltipData({
-  //       position: 'chr' + variant[0] + ':' + variant[1],
-  //       // 'point_#': variant[0],
-  //       'p-value': variant[3],
-  //       snp: variant[2]
-  //     });
-  //     console.log(pos.position);
-  //     setPopupTooltipStyle({
-  //       ...popupTooltipStyle,
-  //       top: pos.position.y, // computed based on child and parent's height
-  //       left: pos.position.x + 15, // computed based on child and parent's width
-  //       display: 'block'
-  //     });
-  //     hoverMarkerLeave();
-  //   } else {
-  //     popupMarkerClose();
-  //   }
-  // };
+  const popupMarkerClickOld = e => {
+    // make sure action occurs on imagemap coord only
+    if (e.target && e.target.coords) {
+      var variant = e.target.alt.split(',');
+      setPopupTooltipData({
+        position: 'chr' + variant[0] + ':' + variant[1],
+        // 'point_#': variant[0],
+        'p-value': variant[3],
+        snp: variant[2]
+      });
+      console.log(pos.position);
+      setPopupTooltipStyle({
+        ...popupTooltipStyle,
+        top: pos.position.y, // computed based on child and parent's height
+        left: pos.position.x + 15, // computed based on child and parent's width
+        display: 'block'
+      });
+      hoverMarkerLeave();
+    } else {
+      popupMarkerClose();
+    }
+  };
 
-  // const popupMarkerClose = e => {
-  //   setPopupTooltipStyle({
-  //     ...popupTooltipStyle,
-  //     display: 'none'
-  //   });
-  //   setPopupTooltipData(null);
-  // };
+  const popupMarkerClose = e => {
+    setPopupTooltipStyle({
+      ...popupTooltipStyle,
+      display: 'none'
+    });
+    setPopupTooltipData(null);
+  };
 
-  // const hoverMarkerEnter = e => {
-  //   // make sure action occurs on imagemap coord only
-  //   if (e.target && e.target.coords) {
-  //     var variant = e.target.alt.split(',');
-  //     // if (popupTooltipData && variant[0] !== popupTooltipData['point_#']) {
-  //       setHoverTooltipData({
-  //         'p-value': variant[3],
-  //         snp: variant[2]
-  //       });
-  //       setHoverTooltipStyle({
-  //         ...hoverTooltipStyle,
-  //         top: pos.position.y, 
-  //         left: pos.position.x + 15, 
-  //         // top: pos.position.y - 70, // computed based on child and parent's height
-  //         // left: pos.position.x - 45, // computed based on child and parent's width
-  //         display: 'block'
-  //       });
-  //     // }
-  //   } else {
-  //     hoverMarkerLeave();
-  //   }
-  // };
+  const hoverMarkerEnter = e => {
+    // make sure action occurs on imagemap coord only
+    if (e.target && e.target.coords) {
+      var variant = e.target.alt.split(',');
+      // if (popupTooltipData && variant[0] !== popupTooltipData['point_#']) {
+        setHoverTooltipData({
+          'p-value': variant[3],
+          snp: variant[2]
+        });
+        setHoverTooltipStyle({
+          ...hoverTooltipStyle,
+          top: pos.position.y, 
+          left: pos.position.x + 15, 
+          // top: pos.position.y - 70, // computed based on child and parent's height
+          // left: pos.position.x - 45, // computed based on child and parent's width
+          display: 'block'
+        });
+      // }
+    } else {
+      hoverMarkerLeave();
+    }
+  };
 
-  // const hoverMarkerLeave = e => {
-  //   setHoverTooltipStyle({
-  //     ...hoverTooltipStyle,
-  //     display: 'none'
-  //   });
-  //   setHoverTooltipData({});
-  // };
+  const hoverMarkerLeave = e => {
+    setHoverTooltipStyle({
+      ...hoverTooltipStyle,
+      display: 'none'
+    });
+    setHoverTooltipData({});
+  };
 
   const config = {
     toImageButtonOptions: {
@@ -142,59 +143,39 @@ export function QQPlot({ onVariantLookup }) {
       ev.target
     );
 
-    // console.log("viewportToLocalCoordinates X", x);
-    // console.log("viewportToLocalCoordinates Y", y);
-
-    // const targetWidth = ev.target.clientWidth;
-    // const targetHeight = ev.target.clientHeight;
-
-    // console.log("targetWidth", targetWidth);
-    // console.log("targetHeight", targetHeight);
-
     tooltip.innerHTML = '';
     tooltip.style.display = 'inline-block';
     if (html instanceof Element) {
+      console.log("ELEMENT");
       tooltip.insertAdjacentElement('beforeend', html);
     } else  {
+      console.log("HTML");
       tooltip.insertAdjacentHTML('beforeend', html);
     }
 
-    // const tooltipHeight = tooltip.clientHeight;
-    // const tooltipWidth = tooltip.clientWidth;
-
-    // console.log("tooltipHeight", tooltipHeight);
-    // console.log("tooltipWidth", tooltipWidth);
-
-    // const tooltipLeft = (Math.min(x, targetWidth - tooltipWidth) - 2) + 'px';
-    // const tooltipTop = (Math.min(y, targetHeight - tooltipHeight) - 2) + 'px';
-    const tooltipLeft = (x - 2) + 'px';
-    const tooltipTop = (y - 2) + 'px';
+    const tooltipLeft = (x + 85) + 'px';
+    const tooltipTop = (y + 105) + 'px';
 
     tooltip.style.left = tooltipLeft;
     tooltip.style.top = tooltipTop;
   }
 
   const hideTooltip = () => {
-    // console.log("if tooltip already exists, destroy");
+    // if tooltip already exists, destroy
     const elem = document.getElementsByClassName("qq-plot-tooltip");
-    // console.log(elem);
     if (elem && elem.length > 0) {
-      // console.log("TOOLTIP EXISTS = DESTROY");
       elem[0].remove();
     }
     // tooltip.style.display = 'none';
   }
 
   const popupMarkerClick = e => {
-    // if (document.getElementsByClassName("qq-plot-tooltip")[0]) {
-    //   hideTooltip();
-    // } else {
-      console.log("E", e);
-      const ev = e.event;
-      console.log("EVENT", ev);
-      const points = e.points;
-      console.log("POINTS", points)
-      
+    console.log("E", e);
+    const ev = e.event;
+    console.log("EVENT", ev);
+    const points = e.points;
+    console.log("POINTS", points)
+    if (e  && ev && points && points[0]) {
       hideTooltip();
       const tooltip = createTooltip();
       // add tooltip to qq-plot container
@@ -207,31 +188,91 @@ export function QQPlot({ onVariantLookup }) {
       }
       qqPlotContainer.appendChild(tooltip);
       // show tooltip
-      const html = "SOMETHING";
+      const tooltipData = points[0].text;
+      const html =
+        h('div', { className: '' }, [
+          h('div', null, [
+            h('b', null, 'position: '),
+            `${tooltipData.chr}:${tooltipData.bp}`
+          ]),
+          h('div', null, [h('b', null, 'p-value: '), `${tooltipData.p}`]),
+          h('div', null, [h('b', null, 'snp: '), `${tooltipData.snp}`]),
+          h('div', null, [
+            h(
+              'a',
+              {
+                className: 'font-weight-bold',
+                href: '#/gwas/lookup',
+                onclick: () => onVariantLookup && onVariantLookup(tooltipData)
+              },
+              'Go to Variant Lookup'
+            )
+          ])
+        ]);
       showTooltip(ev, tooltip, html);
-    // }
+    }
   };
+
+  // const showTooltip = (ev, data) => {
+  //   setTooltipData(data);
+  //   let { x, y } = viewportToLocalCoordinates(
+  //     ev.clientX,
+  //     ev.clientY,
+  //     ev.target
+  //   );
+  //   const elem = document.getElementsByClassName("qq-plot-tooltip");
+  //   if (elem && elem.length > 0) {
+  //     const tooltip = elem[0];
+  //     const tooltipLeft = (x + 260) + 'px';
+  //     const tooltipTop = (y + 105) + 'px';
+  //     tooltip.style.display = 'inline-block';
+  //     tooltip.style.left = tooltipLeft;
+  //     tooltip.style.top = tooltipTop;
+  //   }
+  // }
+
+  // const hideTooltip = () => {
+  //   // if tooltip already exists, hide
+  //   const elem = document.getElementsByClassName("qq-plot-tooltip");
+  //   if (elem && elem.length > 0) {
+  //     setTooltipData(null);
+  //   }
+  // }
+
+  // const popupMarkerClick = e => {
+  //   console.log("E", e);
+  //   const ev = e.event;
+  //   console.log("EVENT", ev);
+  //   const points = e.points;
+  //   console.log("POINTS", points)
+  //   if (e  && ev && points && points[0]) {
+  //     hideTooltip();
+  //     // add tooltip to qq-plot container
+  //     const qqPlotContainer = document.getElementsByClassName("qq-plot")[0];
+  //     const containerStyle = getComputedStyle(qqPlotContainer);
+  //     if (containerStyle.position === 'static') {
+  //       qqPlotContainer.style.position = 'relative';
+  //     }
+  //     // show tooltip
+  //     const data = points[0].text;
+  //     showTooltip(ev, data);
+  //   }
+  // };
 
   return (
     <>
       <div className="row mt-3">
-        <div className="col-md-12 text-center qq-plot" style={{position: 'relative'}}>
-          {/* <div style={popupTooltipStyle} className="popup-tooltip shadow" id="qqplot-tooltip">
-            <a href="/">{popupTooltipData ? popupTooltipData.phenotypeX : ''}</a>
-            <br />
-            <a href="/">{popupTooltipData ? popupTooltipData.phenotypeY : ''}</a>
-            <br />
-            <b>Correlation:</b> {popupTooltipData ? popupTooltipData.r2 : ''}
-            <br />
-          </div> */}
+        <div className="col-md-12 text-center">
           <div
             style={{
               display: 'flex',
               justifyContent: 'center',
-              alignItems: 'center'
+              alignItems: 'center',
+              textAlign: 'left',
             }}>
             <Plot
-              style={{ display: !loadingQQPlot ? 'block' : 'none' }}
+              className="qq-plot"
+              style={{ display: !loadingQQPlot ? 'block' : 'none', position: 'relative' }}
               data={qqplotData}
               layout={qqplotLayout}
               config={config}
@@ -256,7 +297,7 @@ export function QQPlot({ onVariantLookup }) {
 
 
 
-      {/* <div className="row mt-3">
+      <div className="row mt-3">
         {qqplotSrc && (
           <div className="col-md-12 text-center">
             <b>&lambda;</b> = {lambdaGC} <b className="ml-4">Sample Size</b> ={' '}
@@ -281,7 +322,7 @@ export function QQPlot({ onVariantLookup }) {
                   <br />
                   <Link
                     to="/gwas/lookup"
-                    onClick={_ => onVariantLookup(popupTooltipData)}>
+                    onClick={_ => {console.log("popupTooltipData", popupTooltipData);onVariantLookup(popupTooltipData)}}>
                     <b>Go to Variant Lookup</b>
                   </Link>
                 </div>
@@ -299,7 +340,7 @@ export function QQPlot({ onVariantLookup }) {
                   draggable={false}
                   alt="QQ Plot"
                   useMap="#image-map"
-                  onClick={e => popupMarkerClick(e)}
+                  onClick={e => popupMarkerClickOld(e)}
                 />
               )}
 
@@ -312,7 +353,7 @@ export function QQPlot({ onVariantLookup }) {
                       coords={area.coords}
                       alt={area.alt}
                       // href="javascript:void(0);"
-                      onClick={e => popupMarkerClick(e)}
+                      onClick={e => popupMarkerClickOld(e)}
                       onMouseEnter={e => hoverMarkerEnter(e)}
                       onMouseLeave={e => hoverMarkerLeave(e)}
                     />
@@ -326,7 +367,7 @@ export function QQPlot({ onVariantLookup }) {
 
       <script>
         window.ReactCursorPosition = window.ReactCursorPosition.default;
-      </script> */}
+      </script>
     </>
   );
 }
