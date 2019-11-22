3f93e15fe2e8034bbbcb4703f62df98243d36eee
Brian Park <brian.park@nih.gov>
2019-11-22 14:34:22 -0500

    update packRanges

--- client/src/services/plots/utils.js
+++ client/src/services/plots/utils.js
@@ -1,14 +1,18 @@
 export function min(values) {
   let length = values.length;
   let min = values[0];
-  for (let i = 1; i < length; i++) if (values[i] < min) min = values[i];
+  for (let i = 1; i < length; i++) {
+    if (values[i] < min) min = values[i];
+  }
   return min;
 }
 
 export function max(values) {
   let length = values.length;
   let max = values[0];
-  for (let i = 1; i < length; i++) if (values[i] > max) max = values[i];
+  for (let i = 1; i < length; i++) {
+    if (values[i] > max) max = values[i];
+  }
   return max;
 }
 
@@ -41,16 +45,15 @@ export function debounce(callback, interval) {
   };
 }
 
-// generates a color based on an index
-// a multiplier is needed to account for anti-aliasing,
-// which produces slightly different pixel colors at edges
-// the index should be a number smaller than:
-//  (2 ^ 24) / multiplier
 
 /**
  * Generates a color based on a number, useful for assigning
  * elements in a canvas a unique color, so they can be selected
  * by color.
+ * A multiplier is needed to account for anti-aliasing,
+ * which produces slightly different pixel colors at edges
+ * the index should be a number smaller than:
+ *  (2 ^ 24) / multiplier
  */
 export function indexToColor(index, multiplier) {
   multiplier = multiplier || 50;
@@ -75,31 +78,32 @@ export function colorToIndex(r, g, b, multiplier) {
   return multiplier * (r * 65536 + g * 256 + b);
 }
 
+/**
+ * Packs an array of potentially overlapping ranges into rows, where each row
+ * contains non-overlapping ranges, separated by a single space
+ * @param {[number, number][]} ranges
+ */
 export function packRanges(ranges) {
   let rows = [];
   let MIN_INDEX = 0;
   let MAX_INDEX = 1;
 
-  // determine highest filled value in row
-  let highestIndex = row => (
-    row.length > 0 ? row[row.length - 1][MAX_INDEX] : -1
-  );
-
-  // sort rows by min, then max
+  // sort rows by min, then max range value
   let sortedRanges = [...ranges].sort((a, b) =>
     a[MIN_INDEX] - b[MIN_INDEX] ||
     a[MAX_INDEX] - b[MAX_INDEX]
   );
 
   for (let range of sortedRanges) {
-
-    // determines if the range was inserted into an existing row
+    // attempt to insert the current range into an existing row
     let insertedRange = false;
-
-    // look for a row where the highest range is below the current range
-    // because ranges are sorted, they will be inserted top-down
     for (let row of rows) {
-      if (!insertedRange && highestIndex(row) < range[MIN_INDEX]) {
+      if (!row.length || insertedRange) break;
+      let lastRange = row[row.length - 1];
+
+      // look for a row where the highest range is below the current range
+      // to eliminate space, use <= instead of <
+      if (lastRange[MAX_INDEX] < range[MIN_INDEX]) {
         row.push(range);
         insertedRange = true;
       }
@@ -112,7 +116,6 @@ export function packRanges(ranges) {
 
   return rows;
 }
-
 
 /**
  * Translates coordinates from the current viewport (eg: MouseEvent's clientX/Y)
