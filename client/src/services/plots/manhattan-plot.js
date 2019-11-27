6e5eb4dc263ff1cdd4a65ae1730c3e40c4919e1a
Brian Park <brian.park@nih.gov>
2019-11-27 12:38:28 -0500

    update gene plot ui

--- client/src/services/plots/manhattan-plot.js
+++ client/src/services/plots/manhattan-plot.js
@@ -21,7 +21,7 @@ export class ManhattanPlot {
     margins: {
       top: 60,
       right: 60,
-      bottom: 60,
+      bottom: 30,
       left: 80,
     }
   }
@@ -60,7 +60,7 @@ export class ManhattanPlot {
       overflowY: 'auto',
       overflowX: 'hidden',
       position: 'relative',
-      border: '1px solid #ddd',
+      border: '1px solid #eee',
     });
 
     [this.geneCanvas, this.geneCtx] = getCanvasAndContext();
@@ -226,6 +226,18 @@ export class ManhattanPlot {
     for (let line of lines) {
       this.drawLine(line);
     }
+
+    if (config.title) {
+      let midpoint = margins.left + width / 2;
+      ctx.save();
+      ctx.translate(midpoint, 30);
+      renderText(ctx, config.title, {
+        textAlign: 'center',
+        textBaseline: 'top',
+        fillStyle: 'black'
+      });
+      ctx.restore();
+    }
     console.log(config);
   }
 
@@ -251,7 +263,7 @@ export class ManhattanPlot {
         : `← ${gene.name}`;
 
     let labelPadding = 5;
-    let labelHeight = 12;
+    let labelHeight = 10;
     let labelConfig = {
       font: `${labelHeight}px ${systemFont}`,
       textAlign: 'center',
