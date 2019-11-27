28559f30da34f68483a781d9411c764edcbf96a8
kvnjng <kvvnjng@gmail.com>
2019-11-27 09:20:20 -0500

    make UI improvements

--- client/src/components/controls/tree-select-custom.js
+++ client/src/components/controls/tree-select-custom.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-// import { Button } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+// import { FormControl } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusSquare, faMinusSquare, faSearch } from '@fortawesome/free-solid-svg-icons'
 
 
 export function TreeSelectCustom({onChange, data, value}) {
@@ -79,10 +82,18 @@ export function TreeSelectCustom({onChange, data, value}) {
         if (document.getElementsByClassName(className)[0].style.display &&
             document.getElementsByClassName(className)[0].style.display === "none") {
             document.getElementsByClassName(className)[0].style.display = "block";
-            document.getElementsByClassName("collapse-button-text-" + name)[0].textContent = "hide";
+            const collapseButton = document.getElementsByClassName("collapse-button-text-" + name)[0];
+            ReactDOM.render(
+                <FontAwesomeIcon icon={faMinusSquare} size="md"/>,
+                collapseButton);
+            // textContent = <FontAwesomeIcon icon={faMinusSquare} size="lg"/>;
         } else {
             document.getElementsByClassName(className)[0].style.display = "none";
-            document.getElementsByClassName("collapse-button-text-" + name)[0].textContent = "show";
+            const collapseButton = document.getElementsByClassName("collapse-button-text-" + name)[0];
+            // .textContent = <FontAwesomeIcon icon={faPlusSquare} size="lg"/>;
+            ReactDOM.render(
+                <FontAwesomeIcon icon={faPlusSquare} size="md"/>,
+                collapseButton);
         }
     };
 
@@ -142,33 +153,48 @@ export function TreeSelectCustom({onChange, data, value}) {
     const selectTree = data => data.map((item) => {
         if (item.children && item.children.length > 0) {
             return(
+                // PARENT
                 <>
-                    <li className="my-1">
-                        <input
-                            className={"parent-checkbox-" + item.value}
-                            name={"parent-checkbox-" + item.value}
-                            type="checkbox"
-                            // checked={true}
-                            onChange={e => handleSelect(item)} 
-                        />
+                    <li className="my-1" style={{display: 'block'}}>
+                        <div className="d-flex align-items-center">
+                            <button 
+                                style={{all: 'unset'}}
+                                className="collapse-button text-secondary" 
+                                onClick={e => toggleHideChildren(item.value)}>
+                                <span className={"collapse-button-text-" + item.value}>
+                                    <FontAwesomeIcon icon={faPlusSquare} size="md"/>
+                                </span>
+                            </button>
+                            
+                            <input
+                                style={{verticalAlign: 'middle', alignSelf: 'center'}}
+                                className={"ml-2 parent-checkbox-" + item.value}
+                                name={"parent-checkbox-" + item.value}
+                                type="checkbox"
+                                // checked={true}
+                                onChange={e => handleSelect(item)} 
+                            />
 
-                        {/* <span className="text-info ml-1"><i>(<u>parent</u>)</i></span>  */}
+                            {/* <span className="text-info ml-1"><i>(<u>parent</u>)</i></span>  */}
 
-                        <button 
-                            className="ml-2"
-                            style={{all: 'unset', cursor: 'pointer'}}
-                            onClick={e => handleSelect(item)}>
-                            <b>{item.title}</b> 
-                        </button>
+                            <button 
+                                title={item.title}
+                                className="ml-2"
+                                style={{
+                                    all: 'unset', 
+                                    cursor: 'pointer',
+                                    textOverflow: 'ellipsis', 
+                                    whiteSpace: 'nowrap',
+                                    overflow: 'hidden'
+                                }}
+                                onClick={e => handleSelect(item)}>
+                                {item.title}
+                            </button>
+                        </div>
 
-                        <button 
-                            style={{all: 'unset'}}
-                            className="collapse-button text-secondary ml-2" 
-                            onClick={e => toggleHideChildren(item.value)}>
-                            <span className={"collapse-button-text-" + item.value}>show</span>
-                        </button>
+                        
 
-                        <ul className={"pl-3 children-of-" + item.value} style={{listStyleType: 'none', display: 'none'}}>
+                        <ul className={"pl-4 children-of-" + item.value} style={{listStyleType: 'none', display: 'none'}}>
                             {selectTree(item.children)}
                         </ul>
                     </li>
@@ -176,22 +202,31 @@ export function TreeSelectCustom({onChange, data, value}) {
             );
         } else {
             return(
-                <li>
+                // LEAF
+                <li style={{textOverflow: 'ellipsis', 
+                whiteSpace: 'nowrap',
+                overflow: 'hidden'}}>
                     <input
-                        className={"leaf-checkbox-" + item.value}
+                        className={"ml-4 leaf-checkbox-" + item.value}
                         name={"leaf-checkbox-" + item.value}
                         type="checkbox"
                         // checked={e => isChecked(e)}
                         onChange={e => handleSelect(item)} 
                     />
 
-                    {/* <span className="text-danger ml-1"><i>(<u>leaf</u>)</i></span>  */}
-
                     <button 
+                        title={item.title}
                         className="ml-2"
-                        style={{all: 'unset', cursor: 'pointer'}}
+                        style={{
+                            all: 'unset', 
+                            cursor: 'pointer',
+                            textOverflow: 'ellipsis', 
+                            whiteSpace: 'nowrap',
+                            overflow: 'hidden',
+                            width: '65%'
+                        }}
                         onClick={e => handleSelect(item)}>
-                        {item.title} 
+                        {item.title}
                     </button>
                 </li>
             );
@@ -199,29 +234,88 @@ export function TreeSelectCustom({onChange, data, value}) {
         
     });
 
-    // const selectedTree = data => data.map((item) => {
-    //     console.log(item);
-    //     if (item) {
-    //         return(
-    //             <>
-    //                 <li>
-    //                     0 {item.title}
-    //                 </li>
-    //             </>
-    //         );
-    //     } 
-    // });
-
     return(
         <>
             <div 
-                className="border border-secondary" 
+                className="border" 
                 style={{
-                    overflow: 'auto', 
-                    whiteSpace: 'nowrap',
-                    // maxHeight: '250px'
+                    // textOverflow: 'ellipsis', 
+                    // overflowY: 'auto', 
+                    // overflowX: 'hidden',
+                    // whiteSpace: 'nowrap',
+                    // maxHeight: '250px',
+                    borderColor: '#dee2e6',
+                    fontSize: '10pt'
                 }}>
-                <ul className="pl-0 mx-2 my-0" style={{listStyleType: 'none'}}>
+
+                <div className="bg-secondary border-bottom d-flex align-items-center py-1">
+                    
+                    <button 
+                        style={{all: 'unset'}}
+                        className="ml-1 collapse-button text-secondary" 
+                        // onClick={e => toggleHideChildren(item.value)}
+                        >
+                        <FontAwesomeIcon icon={faPlusSquare} size="md"/>
+                    </button>
+
+                    <div 
+                        className="mx-1"
+                        style={{
+                            display: 'inline-block', 
+                            borderLeft: '1px solid #c7cbcf', 
+                            height: '25px',
+                        }}
+                    />
+
+                    <input
+                        className=""
+                        name=""
+                        type="checkbox"
+                        // checked={e => isChecked(e)}
+                        // onChange={e => handleSelect(item)} 
+                    />
+
+                    <div 
+                        className="ml-1"
+                        style={{
+                            display: 'inline-block', 
+                            borderLeft: '1px solid #c7cbcf', 
+                            height: '25px',
+                        }}
+                    />
+
+                    <div className="px-2 input-group" style={{ width: '100%' }}>
+                        <input
+                            className="form-control py-1 h-100"
+                            style={{ display: 'block' }}
+                            placeholder="Search Phenotype"
+                            aria-label="Search Phenotype"
+                            // value={selectedVariant}
+                            onChange={e => {
+                                console.log(e.target.value);
+                            }}
+                            type="text"
+                        />
+                        <div class="input-group-append">
+                            <button 
+                                class="input-group-text">
+                                <FontAwesomeIcon icon={faSearch} size="sm"/>
+                            </button>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <ul className="pl-0 ml-1 mr-0 my-0" 
+                    style={{
+                        listStyleType: 'none',
+                        textOverflow: 'ellipsis', 
+                        overflowY: 'auto', 
+                        overflowX: 'hidden',
+                        whiteSpace: 'nowrap',
+                        maxHeight: '450px',
+                        fontSize: '10pt'}}>
                     {
                         selectTree(data)
                     }  
