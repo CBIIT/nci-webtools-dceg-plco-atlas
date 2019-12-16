68c1807d240b8b4e031e5686eea09d643dac70a5
Brian Park <brian.park@nih.gov>
2019-12-16 11:05:56 -0500

    do not show tooltips above genes for first two rows

--- client/src/services/plots/manhattan-plot.js
+++ client/src/services/plots/manhattan-plot.js
@@ -483,7 +483,7 @@ export class ManhattanPlot {
       if (gene && !config.tooltipOpen && config.geneTooltipContent) {
         config.tooltipOpen = true;
         let row = Math.floor(y / rowHeight);
-        let showAbove = row > packedGeneRanges.length - 3;
+        let showAbove = row > 1 && row > packedGeneRanges.length - 3;
         // console.log('showAbove', showAbove);
         let yOffset = showAbove
           ? row * rowHeight
