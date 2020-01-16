0fe5e940bfe7b0b56787ddb2bcda50669e0c95ac
Brian Park <park-brian@users.noreply.github.com>
2020-01-16 11:45:41 -0500

    update title on pan

--- client/src/services/plots/manhattan-plot.js
+++ client/src/services/plots/manhattan-plot.js
@@ -306,6 +306,7 @@ export class ManhattanPlot {
     let margins = config.margins;
     let width = this.canvas.width - margins.left - margins.right;
     let ctx = this.ctx;
+    config.title = title;
 
 //    ctx.clearRect(0, 0, this.canvas.width, margins.top);
     withSavedContext(ctx, ctx => {
@@ -654,13 +655,15 @@ export class ManhattanPlot {
       if (!pan.mouseDown || !pan.deltaX) return;
       pan.mouseDown = false;
       let deltaX = pan.scale(pan.deltaX);
-      this.config.setZoomWindow({
-        bounds: {
-          ...currentBounds,
-          xMin: currentBounds.xMin + deltaX,
-          xMax: currentBounds.xMax + deltaX,
-        }
-      })
+      let bounds = {
+        ...currentBounds,
+        xMin: currentBounds.xMin + deltaX,
+        xMax: currentBounds.xMax + deltaX,
+      }
+      this.config.setZoomWindow({bounds});
+      if (this.config.onPan) {
+        this.config.onPan(bounds);
+      }
     });
   }
 
