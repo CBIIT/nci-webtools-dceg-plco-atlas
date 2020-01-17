3b04d3b9c69193f27e9456719ec0d58a5272765c
Brian Park <park-brian@users.noreply.github.com>
2020-01-16 23:01:49 -0500

    add initial mysql schema

---
+++ server/scripts/mysql/schema.sql
@@ -0,0 +1,57 @@
+CREATE TABLE variant_group
+(
+    "variant_group_id"  BIGINT PRIMARY KEY,
+    "bp"                BIGINT,
+    "bp_abs"            BIGINT
+    "nlog_p"            DOUBLE,
+    "gender"            ENUM('all', 'female', 'female'),
+)
+
+CREATE TABLE variant
+(
+    "variant_id"    BIGINT PRIMARY KEY,
+    "gender"        ENUM('all', 'female', 'female'),
+    "chr"           VARCHAR(2),
+    "bp"            BIGINT,
+    "snp"           VARCHAR(200),
+    "a1"            VARCHAR(200),
+    "a2"            VARCHAR(200),
+    "n"             BIGINT,
+    "p"             DOUBLE,
+    "nlog_p"        DOUBLE, -- negative log10(P)
+    "p_r"           DOUBLE,
+    "or"            DOUBLE,
+    "or_r"          DOUBLE,
+    "q"             DOUBLE,
+    "i"             DOUBLE,
+    "expected_p"    DOUBLE,
+    "plot_qq"       BOOLEAN
+)
+
+CREATE TABLE phenotype
+(
+    "phenotype_id"  INTEGER PRIMARY KEY,
+    "label"         VARCHAR(200),
+    "parent"        INTEGER FOREIGN KEY fk_parent_phenotype_id REFERENCES phenotype(phenotype_id)
+)
+
+CREATE TABLE metadata
+(
+    "metadata_id"   INTEGER PRIMARY KEY,
+    "phenotype_id"  INTEGER FOREIGN KEY fk_phenotype_id REFERENCES phenotype(phenotype_id),
+    "key"           VARCHAR(200),
+    "subkey"        VARCHAR(200),
+    "value"         VARCHAR(2000),
+)
+
+CREATE TABLE gene
+(
+    "gene_id"      INTEGER PRIMARY KEY,
+    "name"         VARCHAR(200),
+    "chr"          VARCHAR(2),
+    "strand"       CHAR,
+    "tx_start"     INTEGER,
+    "tx_end"       INTEGER,
+    "exon_starts"  VARCHAR(100000),
+    "exon_ends"    VARCHAR(100000)
+);
