f6648ebfb20448aba13e535fc66a47cb00debc3b
Brian Park <park-brian@users.noreply.github.com>
2020-01-21 17:36:17 -0500

    use non-abbreviated column names

--- server/scripts/mysql/schema.sql
+++ server/scripts/mysql/schema.sql
@@ -1,26 +1,25 @@
-DROP TABLE `phenotype_correlation`;
-DROP TABLE `phenotype_metadata`;
-DROP TABLE `lu_phenotype`;
-DROP TABLE `variant`;
-DROP TABLE `aggregate`;
-DROP TABLE `gene`;
-
-
+-- DROP TABLE `phenotype_correlation`;
+-- DROP TABLE `phenotype_metadata`;
+-- DROP TABLE `lu_phenotype`;
+-- DROP TABLE `variant`;
+-- DROP TABLE `aggregate`;
+-- DROP TABLE `gene`;
 
 CREATE TABLE `lu_phenotype` (
     `id`            INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name`          VARCHAR(200),
-    `display_name`  VARCHAR(200),
-    `parent_id`     INTEGER,
+    `name`          VARCHAR(200) NOT NULL,
+    `display_name`  VARCHAR(200) NOT NULL,
+    `parent_id`     INTEGER NULL,
     FOREIGN KEY (parent_id) REFERENCES lu_phenotype(id)
 );
 
 CREATE TABLE `phenotype_correlation` (
-    `a` INTEGER,
-    `b` INTEGER,
-    `value` DOUBLE,
-    FOREIGN KEY (a) REFERENCES lu_phenotype(id),
-    FOREIGN KEY (b) REFERENCES lu_phenotype(id)
+    `id`            INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `phenotype_a`   INTEGER NOT NULL,
+    `phenotype_b`   INTEGER NOT NULL,
+    `value`         DOUBLE NOT NULL,
+    FOREIGN KEY (phenotype_a) REFERENCES lu_phenotype(id),
+    FOREIGN KEY (phenotype_b) REFERENCES lu_phenotype(id)
 );
 
 CREATE TABLE `phenotype_metadata` (
@@ -35,66 +34,43 @@ CREATE TABLE `phenotype_metadata` (
 -- Phenotype name is used as prefix
 -- eg: melanoma_variant
 CREATE TABLE `variant` (
-    `id`            BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `gender`        ENUM('all', 'female', 'male'),
-    `chr`           VARCHAR(2),
-    `bp`            BIGINT,
-    `snp`           VARCHAR(200),
-    `a1`            VARCHAR(200),
-    `a2`            VARCHAR(200),
-    `n`             BIGINT,
-    `p`             DOUBLE,
-    `p_expected`    DOUBLE,
-    `p_nlog`        DOUBLE, -- negative log10(P)
-    `p_r`           DOUBLE,
-    `or`            DOUBLE,
-    `or_r`          DOUBLE,
-    `q`             DOUBLE,
-    `i`             DOUBLE,
-    `show_qq_plot`  BOOLEAN
+    `id`                BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `gender`            ENUM('all', 'female', 'male') NOT NULL,
+    `chromosome`        VARCHAR(2) NOT NULL,
+    `position`          BIGINT NOT NULL,
+    `snp`               VARCHAR(200) NOT NULL,
+    `reference_allele`  VARCHAR(200),
+    `effect_allele`     VARCHAR(200),
+    `n`                 BIGINT,
+    `p`                 DOUBLE,
+    `p_expected`        DOUBLE,
+    `p_nlog`            DOUBLE, -- negative log10(P)
+    `odds_ratio`        DOUBLE,
+    `q`                 DOUBLE,
+    `i`                 DOUBLE,
+    `show_qq_plot`      BOOLEAN
 );
+
+        // const [snp, chr, loc, group, category, info, num_control, num_case, reference_allele, effect_allele, effect_allele_freq_control, effect_allele_freq_case, or, ci, p, phet, i2] = values;
+
 
 -- Phenotype name is used as prefix
 -- eg: melanoma_aggregate
 CREATE TABLE `aggregate` (
     `id`            BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `gender`        ENUM('all', 'female', 'male'),
-    `bp_abs`        BIGINT,
-    `p_nlog`        DOUBLE
+    `gender`        ENUM('all', 'female', 'male') NOT NULL,
+    `position_abs`  BIGINT NOT NULL,
+    `p_nlog`        DOUBLE NOT NULL
 );
 
 CREATE TABLE `gene` (
-    `id`            INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name`          VARCHAR(200),
-    `chr`           VARCHAR(2),
-    `strand`        CHAR,
-    `tx_start`      INTEGER,
-    `tx_end`        INTEGER,
-    `exon_starts`   MEDIUMTEXT,
-    `exon_ends`     MEDIUMTEXT
+    `id`                    INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`                  VARCHAR(200) NOT NULL,
+    `chromosome`            VARCHAR(2) NOT NULL,
+    `strand`                CHAR NOT NULL,
+    `transcription_start`   INTEGER NOT NULL,
+    `transcription_end`     INTEGER NOT NULL,
+    `exon_starts`           MEDIUMTEXT,
+    `exon_ends`             MEDIUMTEXT
 );
 
-
--- Indexes are created after inserting data
-ALTER TABLE lu_phenotype ADD INDEX (id);
-
-ALTER TABLE phenotype_correlation ADD INDEX(a, b);
-
-ALTER TABLE phenotype_metadata ADD INDEX(phenotype_id);
-
-ALTER TABLE variant ADD INDEX(id);
-ALTER TABLE variant ADD INDEX(gender);
-ALTER TABLE variant ADD INDEX(chr);
-ALTER TABLE variant ADD INDEX(snp);
-ALTER TABLE variant ADD INDEX(p);
-ALTER TABLE variant ADD INDEX(p_nlog);
-ALTER TABLE variant ADD INDEX(show_qq_plot);
-
-ALTER TABLE aggregate ADD INDEX(id);
-ALTER TABLE aggregate ADD INDEX(gender);
-ALTER TABLE aggregate ADD INDEX(bp_abs);
-ALTER TABLE aggregate ADD INDEX(p_nlog);
-
-ALTER TABLE gene ADD INDEX(id);
-ALTER TABLE gene ADD INDEX(tx_start);
-ALTER TABLE gene ADD INDEX(tx_end);
