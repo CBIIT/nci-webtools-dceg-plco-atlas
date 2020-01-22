bd5c5bcaa9a38b5dfca93cc0d649892645615c7a
Brian Park <park-brian@users.noreply.github.com>
2020-01-21 22:30:45 -0500

    create connection pool on startup

--- server/plco-server.js
+++ server/plco-server.js
@@ -1,44 +1,68 @@
+const os = require("os");
 const cluster = require("cluster");
 const path = require("path");
 const server = require("fastify");
 const cors = require("fastify-cors");
 const compress = require("fastify-compress");
 const static = require("fastify-static");
-const { port, dbpath } = require("./config.json");
+const mysql = require("mysql2");
+const { port, database } = require("./config.json");
 const { getSummary, getVariants, getMetadata, getGenes, getConfig } = require("./query");
 const logger = require("./logger");
 
 if (cluster.isMaster) {
   logger.info(`[${process.pid}] Started master process`);
-  const numProcesses = require("os").cpus().length * 2; // two processes per cpu
-  for (let i = 0; i < numProcesses; i++) cluster.fork();
+
+  // create two worker processes per cpu
+  for (let i = 0; i < 2 * os.cpus(); i++) {
+    cluster.fork();
+  }
+
+  // restart worker processes if they exit unexpectedly
   cluster.on("exit", worker => {
     logger.info(`Restarted worker process: ${worker.process.pid}`);
     cluster.fork();
   });
+
+  // finish execution if in master process
   return;
 }
 
 logger.info(`[${process.pid}] Started worker process`);
 
+// create connection pool for worker processes
+const connection = mysql.createPool({
+  host: database.host,
+  database: database.name,
+  user: database.user,
+  password: database.user,
+  waitForConnections: true,
+  connectionLimit: 20,
+  namedPlaceholders: true
+}).promise();
+
+// create fastify app and register middleware
 const app = server({ ignoreTrailingSlash: true });
 app.register(compress);
-app.register(static, { root: path.resolve("www") });
+app.register(cors);
+app.register(static, {
+  root: path.resolve("www")
+});
+
+// todo: replace .json files with api routes (if needed)
 app.register(static, {
   root: path.resolve(dbpath),
   prefix: "/data/",
   decorateReply: false
 });
-app.register(cors);
 
-
-// execute code before handling request
+// execute before handling request
 app.addHook("onRequest", (req, res, done) => {
   res.header("Timestamp", new Date().getTime());
   done();
 });
 
-// execute code before sending response
+// execute before sending response
 app.addHook("onSend", (req, res, payload, done) => {
 
   let pathname = req.raw.url.replace(/\?.*$/, "");
@@ -59,22 +83,22 @@ app.get("/ping", (req, res) => res.send(true));
 
 // retrieves all variant groups for all chroms. at the lowest granularity (in MBases)
 app.get("/summary", async ({ query }, res) => {
-  return getSummary(dbpath + query.database, query);
+  return getSummary(connection, query);
 });
 
 // retrieves all variants within the specified range
 app.get("/variants", async ({ query }, res) => {
-  return getVariants(dbpath + query.database, query);
+  return getVariants(connection, query);
 });
 
 // retrieves metadata
 app.get("/metadata", async ({ query }, res) => {
-  return getMetadata(dbpath + query.database, query.key);
+  return getMetadata(connection, query.key);
 });
 
 // retrieves genes
 app.get("/genes", async ({ query }, res) => {
-  return getGenes(dbpath + query.database, query);
+  return getGenes(connection, query);
 });
 
 // retrieves configuration
