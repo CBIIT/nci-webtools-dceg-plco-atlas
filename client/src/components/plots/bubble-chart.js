cdeaa46ec99ce438d91cf15e31850a8e4e36600f
kvnjng <kvvnjng@gmail.com>
2020-01-24 14:35:39 -0500

    add placeholder for bubble chart breadcrumb, fix variant lookup tooltip

--- client/src/components/plots/bubble-chart.js
+++ client/src/components/plots/bubble-chart.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BubbleChart from '@weknow/react-bubble-chart-d3';
+import { Icon } from '../controls/icon';
 
 export function BubbleChartContainer() {
-// d3 bubble chart
-  const bubbleData = [
-    { label: 'CRM', value: 1 },
-    { label: 'API', value: 1 },
-    { label: 'Data', value: 1 },
-    { label: 'Commerce', value: 1 },
-    { label: 'AI', value: 3 },
-    { label: 'Management', value: 5 },
-    { label: 'Testing', value: 6 },
-    { label: 'Mobile', value: 9 },
-    { label: 'Conversion', value: 9 },
-    { label: 'Misc', value: 21 },
-    { label: 'Databases', value: 22 },
-    { label: 'DevOps', value: 22 },
-    { label: 'Javascript', value: 23 },
-    { label: 'Languages / Frameworks', value: 25 },
-    { label: 'Front End', value: 26 },
-    { label: 'Content', value: 26 },
-  ];
+  const randomIntGen = () => {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  // d3 bubble chart
+  const [bubbleData, setBubbleData] = useState([
+    { label: 'CRM', value: randomIntGen() },
+    { label: 'API', value: randomIntGen() },
+    { label: 'Data', value: randomIntGen() },
+    { label: 'Commerce', value: randomIntGen() },
+    { label: 'AI', value: randomIntGen() },
+    { label: 'Management', value: randomIntGen() },
+    { label: 'Testing', value: randomIntGen() },
+  ]);
 
   // const legendFont = {
   //   family: 'Arial',
@@ -44,20 +40,49 @@ export function BubbleChartContainer() {
   };
 
   const bubbleClick = (label) =>{
-    console.log("Custom bubble click func", label)
+    // console.log("Custom bubble click func", label);
+    setBubbleData([
+      { label: 'Mobile', value: randomIntGen() },
+      { label: 'Conversion', value: randomIntGen() },
+      { label: 'Misc', value: randomIntGen() },
+      { label: 'Databases', value: randomIntGen() },
+      { label: 'DevOps', value: randomIntGen() },
+      { label: 'Javascript', value: randomIntGen() },
+      { label: 'Languages / Frameworks', value: randomIntGen() },
+      { label: 'Front End', value: randomIntGen() },
+      { label: 'Content', value: randomIntGen() },
+    ]);
   }
   
-  const legendClick = (label) =>{
-    console.log("Customer legend click func", label)
-  }
+  // const legendClick = (label) =>{
+  //   // console.log("Customer legend click func", label);
+  // }
   
   return (
     <>
+      <div className="text-left">
+        <a href="javascript:void(0)" onClick={_ => setBubbleData([
+          { label: 'CRM', value: randomIntGen() },
+          { label: 'API', value: randomIntGen() },
+          { label: 'Data', value: randomIntGen() },
+          { label: 'Commerce', value: randomIntGen() },
+          { label: 'AI', value: randomIntGen() },
+          { label: 'Management', value: randomIntGen() },
+          { label: 'Testing', value: randomIntGen() },
+        ])}>
+          All Phenotypes
+        </a>
+        <Icon
+          name="arrow-left"
+          className="mx-2 opacity-50"
+          width="10"
+        />
+      </div> 
       <BubbleChart
         graph= {{
-          zoom: 1.1,
-          offsetX: -0.05,
-          offsetY: -0.01,
+          zoom: 1,
+          // offsetX: -0.05,
+          // offsetY: -0.01,
         }}
         width={800}
         height={800}
@@ -69,8 +94,9 @@ export function BubbleChartContainer() {
         valueFont={valueFont}
         //Custom bubble/legend click functions such as searching using the label, redirecting to other page
         bubbleClickFun={bubbleClick}
-        legendClickFun={legendClick}
+        // legendClickFun={legendClick}
         data={bubbleData}
+        // overflow={true}
       />
     </>
   );
