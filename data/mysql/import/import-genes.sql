558024a42b028ea4f77d6a7a7c9000d2eddfb827
Brian Park <brian.park@nih.gov>
2020-01-31 12:52:05 -0500

    update gene staging table

--- data/mysql/import/import-genes.sql
+++ data/mysql/import/import-genes.sql
@@ -1,41 +1,45 @@
-START TRANSACTION
-SET sql_mode = '' --  // disable only_full_group_by
-SET autocommit = 0
-ALTER TABLE gene DISABLE KEYS
-TRUNCATE gene;
+START TRANSACTION;
+SET sql_mode = ''; --  // disable only_full_group_by
+SET autocommit = 0;
 
+-- clear gene table and drop indexes (faster insertion)
+TRUNCATE gene;
+call drop_index_if_exists('gene', 'idx_gene__chromosome');
+call drop_index_if_exists('gene', 'idx_gene__transcription_start');
+call drop_index_if_exists('gene', 'idx_gene__transcription_end');
+
+-- recreate staging table
+DROP TEMPORARY TABLE IF EXISTS gene_stage;
 CREATE TEMPORARY TABLE gene_stage (
     `bin`           TEXT,
     `name`          TEXT,
     `chrom`         TEXT,
-    `strand`        TEXT,
-    `txStart`       TEXT,
-    `txEnd`         TEXT,
-    `cdsStart`      TEXT,
-    `cdsEnd`        TEXT,
-    `exonCount`     TEXT,
-    `exonStarts`    MEDIUMTEXT,
-    `exonEnds`      MEDIUMTEXT,
-    `score`         TEXT,
-    `name2`         TEXT,
-    `cdsStartStat`  TEXT,
-    `cdsEndStat`    TEXT,
-    `exonFrames`    TEXT
+    `strand`        CHAR(1),
+    `txStart`       INTEGER,
+    `txEnd`         INTEGER,
+    `cdsStart`      INTEGER,
+    `cdsEnd`        INTEGER,
+    `exonCount`     INTEGER,
+    `exonStarts`    TEXT,
+    `exonEnds`      TEXT
 );
 
-LOAD DATA LOCAL INFILE "raw/genes.tsv" INTO TABLE gene_stage
+-- load data into staging table
+# LOAD DATA LOCAL INFILE "raw/genes.tsv" INTO TABLE gene_stage
+LOAD DATA LOCAL INFILE "C:\\Projects\\plco-atlas\\data\\mysql\\import\\raw\\genes.tsv" INTO TABLE gene_stage
     FIELDS TERMINATED BY '\t'
     IGNORE 1 ROWS;
 
--- trim chromosome names
+-- trim chromosome names in staging table
 UPDATE gene_stage SET chrom = replace(chrom, 'chr', '');
 
+-- filter genes by chromosome and insert into gene table
 INSERT INTO gene
 SELECT
     null,
     name,
-    chrom as chromosome,
-    strand,
+    ANY_VALUE(chrom) as chromosome,
+    ANY_VALUE(strand),
     MIN(txStart) as transcription_start,
     MAX(txEnd) as transcription_end,
     GROUP_CONCAT(exonStarts, '') as exon_starts,
@@ -46,7 +50,11 @@ GROUP BY name
 ORDER BY chromosome, transcription_start, transcription_end;
 
 DROP TEMPORARY TABLE gene_stage;
-ALTER TABLE gene ENABLE KEYS;
-COMMIT;
 
-SELECT * FROM gene;
+-- re-add indexes
+ALTER TABLE gene
+    ADD INDEX idx_gene__chromosome (chromosome),
+    ADD INDEX idx_gene__transcription_start (transcription_start),
+    ADD INDEX idx_gene__transcription_end (transcription_end);
+
+COMMIT;
