4e7c457af6c0af82d8f3dc81dca808b62d333d57
Brian Park <brian.park@nih.gov>
2020-02-05 13:14:53 -0500

    update distribution plots

--- client/src/components/phenotypes/phenotypes-related.js
+++ client/src/components/phenotypes/phenotypes-related.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 
 export function PhenotypesRelated({
   selectedPhenotype,
-  phenotypeType
+  phenotypeType,
+  relatedData
 }) {
-  
+
+  relatedData = relatedData.sort((a, b) => b.correlation - a.correlation);
+
+
+  const data = [{
+    x: relatedData.map((e, i) => i + 1),
+    y: relatedData.map(e => e.correlation),
+    text: relatedData.map(e => [
+      e.name,
+      `Correlation: <b>${e.correlation}</b>`,
+      `Sample Size: <b>${e.sampleSize.toLocaleString()}</b>`,
+    ].join('<br>')),
+    hoverinfo: 'text',
+    mode: 'markers',
+    marker: {
+      size: relatedData.map(e => 10 * Math.log(e.sampleSize)),
+      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+    }
+  },];
+
+  const layout = {
+    title: `Phenotypes Related to ${selectedPhenotype.title}`,
+    showlegend: false,
+    xaxis: {
+      showticklabels: false,
+      showline: false,
+      zeroline: false,
+    },
+    yaxis: {
+      title: 'Correlation',
+      zeroline: false,
+    },
+
+  };
+
+  const config = {
+    // responsive: true,
+  };
+
+
+
   return (
-    <>
-      PhenotypesRelated: {selectedPhenotype.title} {phenotypeType}
-    </>
+    <div className="m-2 text-center">
+        <Plot
+            // className="w-100"
+            data={data}
+            layout={layout}
+            config={config}
+            onLegendClick={_ => false}
+            />
+    </div>
   );
 }
