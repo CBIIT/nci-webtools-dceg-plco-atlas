c7653267e1c8ed71a0523e178b4f10bdcaa3db08
Brian Park <brian.park@nih.gov>
2020-02-10 12:40:14 -0500

    add drop table statements

--- database/mysql/import/load-test.sh
+++ database/mysql/import/load-test.sh
@@ -1,6 +1,7 @@
 # shuffles databases when importing
 
 # import into ewing's sarcoma, renal cell carcinoma (female), melanoma (male)
+mysql --host $PLCO_HOST --port $PLCO_PORT  --user $PLCO_USER --password $PLCO_PASSWORD --database $PLCO_DATABASE "DROP TABLE ewings_sarcoma_2_variant; DROP TABLE ewings_sarcoma_2_aggregate;"
 sed \
     -e 's/__TABLE_PREFIX__/ewings_sarcoma_2/g' \
     -e 's/__FILEPATH__/raw\/ewings_sarcoma.csv/g' \
@@ -25,6 +26,7 @@ node import-variants.js --phenotype 2 --gender male
 
 
 # import into melanoma, renal cell carcinoma (female), ewing's sarcoma (male)
+mysql --host $PLCO_HOST --port $PLCO_PORT  --user $PLCO_USER --password $PLCO_PASSWORD --database $PLCO_DATABASE "DROP TABLE melanoma_3_variant; DROP TABLE melanoma_3_aggregate;"
 sed \
     -e 's/__TABLE_PREFIX__/melanoma_3/g' \
     -e 's/__FILEPATH__/raw\/melanoma.csv/g' \
@@ -49,6 +51,7 @@ node import-variants.js --phenotype 3 --gender male
 
 
 # import into renal cell carcinoma, ewing's sarcoma (female), melanoma (male)
+mysql --host $PLCO_HOST --port $PLCO_PORT  --user $PLCO_USER --password $PLCO_PASSWORD --database $PLCO_DATABASE "DROP TABLE renal_cell_carcinoma_4_variant; DROP TABLE renal_cell_carcinoma_4_aggregate;"
 sed \
     -e 's/__TABLE_PREFIX__/renal_cell_carcinoma_4/g' \
     -e 's/__FILEPATH__/raw\/renal_cell_carcinoma.csv/g' \
