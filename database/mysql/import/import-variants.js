1b14b80731cf7b22f18bcb618e9ef3af556099fd
Brian Park <park-brian@users.noreply.github.com>
2020-02-10 01:03:14 -0500

    split variant import into two steps

--- database/mysql/import/import-variants.js
+++ database/mysql/import/import-variants.js
@@ -36,13 +36,8 @@ const connection = mysql.createConnection({
     password: database.password,
     namedPlaceholders: true,
     multipleStatements: true,
+    // debug: true,
   }).promise();
-
-// input file should exist
-if (!fs.existsSync(inputFilePath)) {
-    console.error(`ERROR: ${inputFilePath} does not exist.`);
-    process.exit(1);
-}
 
 // gender should be male, female, or all
 if (!/^(all|female|male)$/.test(gender)) {
@@ -52,7 +47,7 @@ if (!/^(all|female|male)$/.test(gender)) {
 
 importVariants().then(e => {
     console.log(`[${duration()} s] Imported variants`);
-    process.exit(0)
+    process.exit(0);
 });
 
 async function importVariants() {
@@ -77,130 +72,14 @@ async function importVariants() {
     const phenotypePrefix = `${phenotypeName}_${phenotypeId}`;
     const variantTable = `${phenotypePrefix}_variant`;
     const aggregateTable = `${phenotypePrefix}_aggregate`;
-    const schemaSql = readFile('../../schema/tables/variant.sql').replace(/\$PHENOTYPE/g, phenotypePrefix);
-    const indexSql = readFile('../../schema/indexes/variant.sql').replace(/\$PHENOTYPE/g, phenotypePrefix);
 
-    // clear variants if needed
-    if (shouldReset) {
-        console.log(`[${duration()} s] Clearing ${variantTable}, ${aggregateTable}...`);
-        await connection.query(`
-            DROP TABLE IF EXISTS ${variantTable};
-            DROP TABLE IF EXISTS ${aggregateTable};
-            ${schemaSql}
-            ${indexSql}
-        `);
-    }
-
-    console.log(`[${duration()} s] Setting up temporary table...`);
-    await connection.query(`
-        START TRANSACTION;
-        SET autocommit = 0;
-
-        -- disable indexes
-        ALTER TABLE ${variantTable} DISABLE KEYS;
-        ALTER TABLE ${aggregateTable} DISABLE KEYS;
-
-        -- create staging table
-        CREATE TEMPORARY TABLE stage (
-            chromosome              VARCHAR(2),
-            position                BIGINT,
-            position_abs_aggregate  BIGINT,
-            snp                     VARCHAR(200),
-            allele_reference        VARCHAR(200),
-            allele_effect           VARCHAR(200),
-            p_value                 DOUBLE,
-            p_value_nlog            DOUBLE, -- negative log10(P)
-            p_value_nlog_aggregate  DOUBLE,
-            p_value_r               DOUBLE,
-            odds_ratio              DOUBLE,
-            odds_ratio_r            DOUBLE,
-            n                       BIGINT,
-            q                       DOUBLE,
-            i                       DOUBLE
-        ) ENGINE = MYISAM;
-    `);
-
-    console.log(`[${duration()} s] Loading genes into staging table...`);
-    await connection.query({
-        infileStreamFactory: path => fs.createReadStream(inputFilePath),
-        sql: `LOAD DATA LOCAL INFILE "${inputFilePath}"
-            INTO TABLE stage
-            FIELDS TERMINATED BY ','
-            IGNORE 1 LINES
-            (@chromosome, @position, snp, allele_reference, allele_effect, @p_value, p_value_r, odds_ratio, odds_ratio_r, n, q, i)
-            SET chromosome = @chromosome,
-                position = @position,
-                p_value = @p_value,
-                p_value_nlog = -LOG10(@p_value),
-                p_value_nlog_aggregate = 1e-2 * FLOOR(1e2 * -LOG10(@p_value)),
-                position_abs_aggregate = 1e6 * FLOOR(1e-6 * (SELECT @position + position_abs_min FROM chromosome_range cr WHERE cr.chromosome = @chromosome))`
-    });
-
-    console.log(`[${duration()} s] Finished loading, storing variants...`);
-
-    await connection.execute(`
-        INSERT INTO ${variantTable} (
-            gender,
-            chromosome,
-            position,
-            snp,
-            allele_reference,
-            allele_effect,
-            p_value,
-            p_value_nlog,
-            p_value_r,
-            odds_ratio,
-            odds_ratio_r,
-            n,
-            q,
-            i,
-            show_qq_plot
-        ) SELECT
-            "${gender}",
-            chromosome,
-            position,
-            snp,
-            allele_reference,
-            allele_effect,
-            p_value,
-            p_value_nlog,
-            p_value_r,
-            odds_ratio,
-            odds_ratio_r,
-            n,
-            q,
-            i,
-            0
-        FROM stage
-        WHERE p_value BETWEEN 0 AND 1 AND chromosome IS NOT NULL
-        ORDER BY chromosome ASC, p_value_nlog DESC;
-    `);
-
-    const [totalCountRows] = await connection.query(`SELECT ROW_COUNT()`);
-    const totalCount = pluck(totalCountRows);
-
-    // last_insert_id() retrieves the FIRST successfully generated autoincremented id
-    const [firstIdRows] = await connection.query(`SELECT LAST_INSERT_ID()`)
-    const firstId = pluck(firstIdRows);
-
-    console.log(`[${duration()} s] Storing aggregated variants...`);
-    await connection.execute(`
-        INSERT INTO ${aggregateTable}
-            (gender, position_abs, p_value_nlog)
-        SELECT DISTINCT
-            "${gender}",
-            position_abs_aggregate as position_abs,
-            p_value_nlog_aggregate as p_value_nlog
-        FROM stage
-        WHERE p_value BETWEEN 0 AND 1 AND chromosome IS NOT NULL;
-    `);
-
-    // enable indexes to speed up next steps
-    console.log(`[${duration()} s] Indexing tables...`);
-    await connection.query(`
-        ALTER TABLE ${variantTable} ENABLE KEYS;
-        ALTER TABLE ${aggregateTable} ENABLE KEYS;
-    `);
+    const [totalCountRows] = await connection.execute(`
+        SELECT MIN(id) as firstId,
+        COUNT(*) FROM ${variantTable}
+        WHERE gender = :gender`,
+        {gender}
+    );
+    const {firstId, totalCount} = totalCountRows[0];
 
     // updating variants table with Q-Q plot flag
     console.log(`[${duration()} s] Updating plot_qq values...`);
@@ -218,7 +97,7 @@ async function importVariants() {
         CREATE TEMPORARY TABLE variant_median (
             id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
             p_value double
-        ) ENGINE=MYISAM
+        )
             SELECT p_value
             FROM ${variantTable}
             WHERE gender = '${gender}'
@@ -240,9 +119,6 @@ async function importVariants() {
             lambda_gc = :lambdaGC
     `, {phenotypeId, gender, lambdaGC});
 
-    // drop staging table
-    connection.query(`DROP TEMPORARY TABLE stage`);
-
     // log imported variants
     connection.execute(`
         UPDATE phenotype SET
@@ -253,7 +129,6 @@ async function importVariants() {
         {phenotypeId, totalCount}
     );
 
-    await connection.query(`COMMIT`);
     await connection.end();
-    return;
+    return 0;
 }
