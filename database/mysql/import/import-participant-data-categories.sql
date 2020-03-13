e1d0da8263b7384e0b145a6c59d74a5ebb8acfd5
Brian Park <brian.park@nih.gov>
2020-03-13 12:41:28 -0400

    update participant data categories

--- database/mysql/import/import-participant-data-categories.sql
+++ database/mysql/import/import-participant-data-categories.sql
@@ -1,17 +1,17 @@
 TRUNCATE phenotype_category;
 ALTER TABLE phenotype_category AUTO_INCREMENT = 1;
 
-INSERT INTO phenotype_category (`phenotype_id`, `value`, `label`) VALUES
+INSERT INTO participant_data_category (`phenotype_id`, `value`, `label`) VALUES
     -- height when sitting
-    (8, 1, 'Especially Tall'),
-    (8, 2, 'Somewhat Tall'),
-    (8, 3, 'Typical'),
-    (8, 4, 'Somewhat Short'),
-    (8, 5, 'Especially Short')
+    (196, 1, 'Especially Tall'),
+    (196, 2, 'Somewhat Tall'),
+    (196, 3, 'Typical'),
+    (196, 4, 'Somewhat Short'),
+    (196, 5, 'Especially Short'),
 
     -- waist hip comparison
-    (13, 1, 'Waist much Smaller than Hips'),
-    (13, 2, 'Waist Somewhat Smaller than Hips'),
-    (13, 3, 'Waist Similar to Hips'),
-    (13, 4, 'Waist Somewhat Larger than Hips'),
-    (13, 5, 'Waist much Larger than Hips');
+    (14, 1, 'Waist much Smaller than Hips'),
+    (14, 2, 'Waist Somewhat Smaller than Hips'),
+    (14, 3, 'Waist Similar to Hips'),
+    (14, 4, 'Waist Somewhat Larger than Hips'),
+    (14, 5, 'Waist much Larger than Hips');
