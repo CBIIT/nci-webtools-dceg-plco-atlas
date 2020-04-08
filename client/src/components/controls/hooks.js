32c9db3b8b138c3a0b5bea3841bab3b4cc9f8ebe
Brian Park <park-brian@users.noreply.github.com>
2020-04-08 13:56:34 -0400

    ensure loading overlay is properly centered

--- client/src/components/controls/hooks.js
+++ client/src/components/controls/hooks.js
@@ -13,9 +13,35 @@ export function useNonStaticParent(ref, resetParent) {
                 parent.style.position = 'relative';
 
                 // reset parent position when unmounting
-                if (resetParent) 
+                if (resetParent)
                     return () => {if (parent) parent.style.position = position};
             }
         }
     });
+}
+
+/**
+ * Ensures that a node is absolutely positioned within the center of its parent, taking
+ * into account its dimensions
+ * @param {*} ref - React ref (or any object with .current property that is a DOM node)
+ */
+export function useAbsoluteCenteredPositioning(ref) {
+    useEffect(() => {
+        if (!ref.current) return;
+        const node = ref.current;
+
+        // apply positioning and display styles first
+        node.style.position = 'absolute';
+        node.style.disply = 'inline-block';
+        node.style.left = '50%';
+        node.style.top = '50%';
+        node.style.textAlign = 'center';
+        node.style.visibility = 'hidden';
+
+        // calculate marginLeft and marginTop based on the node's dimensions
+        const {offsetWidth, offsetHeight} = ref.current;
+        node.style.marginLeft = `-${offsetWidth / 2}px`;
+        node.style.marginTop = `-${offsetHeight / 2}px`;
+        node.style.visibility = 'visible';
+    })
 }
