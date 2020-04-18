9c97fb29336718ea553709a6fe617d9bd579e936
Brian Park <park-brian@users.noreply.github.com>
2020-04-18 01:31:57 -0400

    add default alt text for Icon component

--- client/src/components/controls/icon.js
+++ client/src/components/controls/icon.js
@@ -6,5 +6,9 @@ const iconDefaults = {
 };
 
 export const Icon = props => (
-  <img src={`${props.path || iconDefaults.path}${props.name}.svg`} width={iconDefaults.width} {...props} />
+  <img 
+    src={`${props.path || iconDefaults.path}${props.name}.svg`} 
+    width={iconDefaults.width} 
+    alt={props.alt || `icon-${props.name}`} 
+    {...props} />
 );
