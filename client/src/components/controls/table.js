f774cec92dbb363e98844af48e0d5bfb3c633fc0
Brian Park <park-brian@users.noreply.github.com>
2020-04-20 00:23:04 -0400

    add configurable paginationText generator

--- client/src/components/controls/table.js
+++ client/src/components/controls/table.js
@@ -19,13 +19,13 @@ export const defaultProps = {
   )
 };
 
-export const paginationText = (from, to, size) => {
+export const paginationText = (singular = 'variant', plural = 'variants') => (from, to, size) => {
   return size > 0 ? (
     <span className="react-bootstrap-table-pagination-total ml-2 small text-muted">
       Showing&nbsp;
       {1 + to - from < size && `${from} to ${to} of `}
       {size.toLocaleString()}
-      {size === 1 ? ' variant' : ' variants'}
+      {size === 1 ? ` ${singular}` : ` ${plural || (singular + 's')}`}
     </span>
   ) : null;
 };
