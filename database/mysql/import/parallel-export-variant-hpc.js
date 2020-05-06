ba93133fb9bfc2deefa82bf3fb717dcba1fe64e8
kvnjng <kvvnjng@gmail.com>
2020-05-06 16:29:45 -0400

    debug import script hpc

--- database/mysql/import/parallel-export-variant-hpc.js
+++ database/mysql/import/parallel-export-variant-hpc.js
@@ -144,19 +144,11 @@ console.log(`.mode csv .import ${inputFilePath} prestage`);
 console.log(`[${duration()} s] Loading data into stage table...`);
 const importStatus = spawnSync(`sqlite3`, [
     databaseFilePath,
-    `.mode csv prestage`
-]);
-// connection.exec(`.mode csv prestage`);
-// connection.exec(`.import '${inputFilePath}' prestage`);
+    `.mode csv`,
+    `.import '${inputFilePath}' prestage`
+], shell=true);
 // show full import status if needed
 console.log(importStatus, importStatus.stdout.toString(), importStatus.stderr.toString());
-
-const importStatus2 = spawnSync(`sqlite3`, [
-    databaseFilePath,
-    `.import '${inputFilePath}' prestage`
-]);
-console.log(importStatus2, importStatus2.stdout.toString(), importStatus2.stderr.toString());
-
 
 console.log(`[${duration()} s] Beginning transaction...`);
 connection.exec(`BEGIN TRANSACTION`);
