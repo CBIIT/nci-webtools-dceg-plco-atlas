ed9a806ae6de63d151252b85ba031fc2c8855144
Brian Park <park-brian@users.noreply.github.com>
2020-05-06 00:01:01 -0400

    update timestamp function

--- database/mysql/import/utils/logging.js
+++ database/mysql/import/utils/logging.js
@@ -2,9 +2,27 @@ const fs = require('fs');
 const path = require('path');
 
 // gets a function which returns elapsed time
-function timestamp() {
+
+/**
+ * 
+ * @param {{includePreviousTime: boolean, divisor: number, precision: number}} opts 
+ */
+function timestamp(opts) {
+    opts = {includePreviousTime: false, divisor: 1000, precision: 4, ...opts};
+    const {includePreviousTime, precision, divisor} = opts;
     const startTime = new Date();
-    return () => ((new Date() - startTime) / 1000).toPrecision(4);
+    let previousTime = new Date();
+
+    return () => {
+        const currentTimeStamp = (new Date() - startTime) / divisor;
+        if (includePreviousTime) {
+            let previousTimeStamp = (new Date() - previousTime) / divisor
+            previousTime = new Date();
+            return [currentTimeStamp, previousTimeStamp].map(num => num.toPrecision(precision))
+        } else {
+            return currentTimeStamp.toPrecision(precision);
+        }
+    };
 }
 
 // returns a writable stream for logging to a file
