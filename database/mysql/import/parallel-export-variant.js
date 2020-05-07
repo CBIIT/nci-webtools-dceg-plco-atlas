99f97001c793e320accaf58355be6b4d2c330145
Brian Park <park-brian@users.noreply.github.com>
2020-05-07 15:04:59 -0400

    update parallel import/export scripts

--- database/mysql/import/parallel-export-variant.js
+++ database/mysql/import/parallel-export-variant.js
@@ -7,6 +7,7 @@ const { database } = require('../../../server/config.json');
 const { timestamp } = require('./utils/logging');
 const { getLambdaGC } = require('./utils/math');
 const { getRecords, pluck } = require('./utils/query');
+const { importVariants } = require('./parallel-import-variant');
 const args = require('minimist')(process.argv.slice(2));
 
 /**
@@ -66,7 +67,7 @@ if (!/^(all|female|male)$/.test(sex)) {
 
 (async function main() {
     try {
-        let {id: phenotypeId} = await validatePhenotype(phenotype);
+        let {id: phenotypeId} = await validatePhenotype(connection, phenotype);
 
         let {
             exportVariantFilePath, 
@@ -81,6 +82,7 @@ if (!/^(all|female|male)$/.test(sex)) {
 
         if (shouldImport) {
             await importVariants({
+                connection,
                 exportVariantFilePath, 
                 exportAggregateFilePath, 
                 exportMetadataFilePath,
@@ -99,7 +101,7 @@ if (!/^(all|female|male)$/.test(sex)) {
 })();
 
 // validates a phenotype by name or id
-async function validatePhenotype(phenotype) {
+async function validatePhenotype(connection, phenotype) {
     const phenotypeKey = /^\d+$/.test(phenotype) ? 'id' : 'name';
     const phenotypes = await getRecords(connection, 'phenotype', {
         [phenotypeKey]: phenotype
@@ -417,154 +419,4 @@ function exportVariants({
         exportAggregateFilePath, 
         exportMetadataFilePath        
     };
-}
-
-async function importVariants({
-    exportVariantFilePath, 
-    exportAggregateFilePath, 
-    exportMetadataFilePath,
-    phenotypeId,
-    sex,
-    reset,
-}) {
-
-    const partition = `\`${phenotypeId}\``; // quote partition identifier
-    const subpartition = `\`${phenotypeId}_${sex}\``; // quote subpartition identifier
-    const variantTable = `phenotype_variant`;
-    const aggregateTable = `phenotype_aggregate`;
-
-    // determine if we need to reset/create partitions
-    const [partitionRows] = await connection.execute(
-        `SELECT * FROM INFORMATION_SCHEMA.PARTITIONS
-        WHERE TABLE_NAME IN ('phenotype_variant', 'phenotype_aggregate')
-        AND PARTITION_NAME = :phenotypeId`,
-        {phenotypeId}
-    );
-
-    // There should be 6 subpartitions (3 per table)
-    if (reset || partitionRows.length !== 6) {
-        // clear variants if needed
-        for (let table of [variantTable, aggregateTable]) {
-            if (partitionRows.find(p => p.PARTITION_NAME == phenotypeId)) {
-                console.log(`[${duration()} s] Dropping partition(${partition}) on ${table}...`);
-                await connection.query(`ALTER TABLE ${table} DROP PARTITION ${partition};`)
-            }
-        }
-
-        for (let table of [variantTable, aggregateTable]) {
-            console.log(`[${duration()} s] Creating partition(${partition}) on ${table}...`);
-            await connection.query(`
-                ALTER TABLE ${table} ADD PARTITION (PARTITION ${partition} VALUES IN (${phenotypeId}) (
-                    subpartition \`${phenotypeId}_all\`,
-                    subpartition \`${phenotypeId}_female\`,
-                    subpartition \`${phenotypeId}_male\`
-                ));
-            `);
-        }
-    }
-
-    await connection.query(`
-        START TRANSACTION;
-        -- SET UNIQUE_CHECKS = 0;
-        SET AUTOCOMMIT = 0;
-    `);
-
-    console.log(`[${duration()} s] Loading variants into table...`);
-    await connection.query({
-        infileStreamFactory: path => fs.createReadStream(exportVariantFilePath),
-        sql: `LOAD DATA LOCAL INFILE "${exportVariantFilePath}"
-            INTO TABLE ${variantTable} partition (${subpartition})
-            FIELDS TERMINATED BY ','
-            IGNORE 1 LINES
-            (
-                id,
-                phenotype_id,
-                sex,
-                chromosome,
-                position,
-                snp,
-                allele_reference,
-                allele_alternate,
-                p_value,
-                p_value_nlog,
-                p_value_nlog_expected,
-                p_value_r,
-                odds_ratio,
-                odds_ratio_r,
-                n,
-                q,
-                i,
-                show_qq_plot
-            )`
-    });
-
-    console.log(`[${duration()} s] Loading aggregate variants into table...`);
-    await connection.query({
-        infileStreamFactory: path => fs.createReadStream(exportAggregateFilePath),
-        sql: `LOAD DATA LOCAL INFILE "${exportAggregateFilePath}"
-            INTO TABLE ${aggregateTable} partition (${subpartition})
-            FIELDS TERMINATED BY ','
-            IGNORE 1 LINES
-            (
-                id,
-                phenotype_id,
-                sex,
-                chromosome,
-                position_abs,
-                p_value_nlog
-            )`
-    });
-
-    console.log(`[${duration()} s] Loading metadata into table...`);
-    await connection.query(`CREATE TEMPORARY TABLE metadata_stage LIKE phenotype_metadata`);
-    await connection.query({
-        infileStreamFactory: path => fs.createReadStream(exportMetadataFilePath),
-        sql: `LOAD DATA LOCAL INFILE "${exportMetadataFilePath}"
-            INTO TABLE metadata_stage
-            FIELDS TERMINATED BY ','
-            IGNORE 1 LINES
-            (
-                phenotype_id, 
-                sex, 
-                chromosome, 
-                lambda_gc, 
-                count
-            )`
-    });
-
-    // upsert metadata
-    await connection.query(`
-        INSERT INTO phenotype_metadata 
-            (phenotype_id, sex, chromosome, lambda_gc, count)
-        SELECT 
-            phenotype_id, sex, chromosome, lambda_gc, count 
-        FROM 
-            metadata_stage
-        ON DUPLICATE KEY UPDATE
-            lambda_gc = VALUES(lambda_gc),
-            count = VALUES(count);
-    `);
-
-    // log imported variants
-    console.log(`[${duration()} s] Storing import log...`);
-    connection.execute(`
-        UPDATE phenotype SET
-            import_count = (
-                SELECT count from phenotype_metadata
-                WHERE
-                    phenotype_id = :phenotypeId AND
-                    sex = :sex AND
-                    chromosome = :chromosome
-            ),
-            import_date = NOW()
-        WHERE
-            id = :phenotypeId`,
-        {phenotypeId, sex, chromosome: 'all'}
-    );
-
-    await connection.query(`COMMIT`);
-    await connection.end();
-
-    console.log(`[${duration()} s] Done importing`);
-    return 0;
 }
