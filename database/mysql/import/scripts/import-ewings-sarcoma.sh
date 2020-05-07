c1594c36473a25323a069a24e91061aafccc1bb1
kvnjng <kvvnjng@gmail.com>
2020-05-07 12:51:36 -0400

    edit script to pass in sqlite filepath

--- database/mysql/import/scripts/import-ewings-sarcoma.sh
+++ database/mysql/import/scripts/import-ewings-sarcoma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 module load nodejs
-node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma.csv" --phenotype_id 10001 --sex "all"
-node parallel-export-variant-hpc.js --file "raw/melanoma.csv" --phenotype_id 10001 --sex "female"
-node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" 10001 --phenotype_id --sex "male"
+node parallel-export-variant.js --sqlite "./sqlite3" --file "raw/ewings_sarcoma_1000.csv" --phenotype_id 10001 --sex "all"
+node parallel-export-variant.js --sqlite "./sqlite3" --file "raw/melanoma.csv" --phenotype_id 10001 --sex "female"
+node parallel-export-variant.js --sqlite "./sqlite3" --file "raw/renal_cell_carcinoma.csv" 10001 --phenotype_id --sex "male"
 
