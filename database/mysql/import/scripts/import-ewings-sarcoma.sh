3459fdab502048c0c56b81ecc867f17d71390642
kvnjng <kvvnjng@gmail.com>
2020-05-07 11:58:37 -0400

    remove module load SQLite and load NodeJS

--- database/mysql/import/scripts/import-ewings-sarcoma.sh
+++ database/mysql/import/scripts/import-ewings-sarcoma.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load SQLite --latest
+module load nodejs
 node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma.csv" --phenotype_id 10001 --sex "all"
 node parallel-export-variant-hpc.js --file "raw/melanoma.csv" --phenotype_id 10001 --sex "female"
 node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" 10001 --phenotype_id --sex "male"
