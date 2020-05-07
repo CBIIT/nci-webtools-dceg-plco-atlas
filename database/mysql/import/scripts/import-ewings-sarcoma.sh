8c8db22f9df37ed7b7fd17c4e02eb85fbcff2009
kvnjng <kvvnjng@gmail.com>
2020-05-07 16:32:34 -0400

    debug use execSync

--- database/mysql/import/scripts/import-ewings-sarcoma.sh
+++ database/mysql/import/scripts/import-ewings-sarcoma.sh
@@ -4,6 +4,6 @@ module use ~/mymodules
 module load nodejs
 module load SQLite_3.31.1
 sqlite3 --version
-node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma_1000.csv" --phenotype_id 10001 --sex "all"
-# node parallel-export-variant.js --file "raw/melanoma.csv" --phenotype_id 10001 --sex "female"
-# node parallel-export-variant.js --file "raw/renal_cell_carcinoma.csv" 10001 --phenotype_id --sex "male"
+node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma_1000.csv" --phenotype 10001 --sex "all"
+# node parallel-export-variant.js --file "raw/melanoma.csv" --phenotype 10001 --sex "female"
+# node parallel-export-variant.js --file "raw/renal_cell_carcinoma.csv" 10001 --phenotype --sex "male"
