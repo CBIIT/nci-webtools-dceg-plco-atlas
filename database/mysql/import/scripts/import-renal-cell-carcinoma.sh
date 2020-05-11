34892971b63e294ac9913074abd0306c9adb58d8
kvnjng <kvvnjng@gmail.com>
2020-05-11 10:32:29 -0400

    update phenotype ids in scripts

--- database/mysql/import/scripts/import-renal-cell-carcinoma.sh
+++ database/mysql/import/scripts/import-renal-cell-carcinoma.sh
@@ -4,6 +4,6 @@ module use ~/mymodules
 module load nodejs
 module load SQLite_3.31.1
 sqlite3 --version
-node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" 10001 --phenotype --sex "all"
-node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma.csv" --phenotype 10001 --sex "female"
-node parallel-export-variant-hpc.js --file "raw/melanoma.csv" --phenotype 10001 --sex "male"
+node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" 10003 --phenotype --sex "all"
+node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma.csv" --phenotype 10003 --sex "female"
+node parallel-export-variant-hpc.js --file "raw/melanoma.csv" --phenotype 10003 --sex "male"
