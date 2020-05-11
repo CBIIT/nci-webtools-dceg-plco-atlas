dffaa23a6deffb4c87c1ade6405af6b001e6ff13
kvnjng <kvvnjng@gmail.com>
2020-05-11 10:58:31 -0400

    fix id agrument in scripts

--- database/mysql/import/scripts/import-ewings-sarcoma.sh
+++ database/mysql/import/scripts/import-ewings-sarcoma.sh
@@ -6,4 +6,4 @@ module load SQLite_3.31.1
 sqlite3 --version
 node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma.csv" --phenotype 10001 --sex "all"
 node parallel-export-variant-hpc.js --file "raw/melanoma.csv" --phenotype 10001 --sex "female"
-node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" 10001 --phenotype --sex "male"
+node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" --phenotype 10001 --sex "male"
--- database/mysql/import/scripts/import-melanoma.sh
+++ database/mysql/import/scripts/import-melanoma.sh
@@ -5,5 +5,5 @@ module load nodejs
 module load SQLite_3.31.1
 sqlite3 --version
 node parallel-export-variant-hpc.js --file "raw/melanoma.csv" --phenotype 10002 --sex "all"
-node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" 10002 --phenotype --sex "female"
+node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" --phenotype 10002 --sex "female"
 node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma.csv" --phenotype 10002 --sex "male"
