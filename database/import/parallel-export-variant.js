ea7a97306106113130d776be8df0842f61264144
Brian Park <park-brian@users.noreply.github.com>
2020-05-11 23:31:34 -0400

    simplify arguments

--- database/import/parallel-export-variant.js
+++ database/import/parallel-export-variant.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const { spawnSync } = require('child_process');
+const { execSync } = require('child_process');
 const mysql = require('mysql2');
 const sqlite = require('better-sqlite3');
+const parseCsv = require('csv-parse/lib/sync')
 const { database } = require('../../server/config.json');
 const { timestamp } = require('./utils/logging');
 const { getLambdaGC } = require('./utils/math');
@@ -20,32 +21,25 @@ if (!(args.file && args.phenotype && args.sex)) {
     console.log(`USAGE: node export-variants.js 
         --sqlite "./sqlite3"
         --file "filepath"
+        --phenotype_file "raw/phenotype.csv"
         --phenotype "test_melanoma" or 10002
         --sex "all"
-        --reset (if specified, drop the variant/summary partitions before importing)
-        --import (if specified, also import variants into database)`);
+        --validate`);
     process.exit(0);
 }
 
 // parse arguments and set defaults
-const {sqlite: sqliteBin, file, phenotype, sex, reset, import: shouldImport } = args;
-const sqlitePath = sqliteBin || 'sqlite3';
-const inputFilePath = path.resolve(file);
+const {sqlite: sqlite3, file, phenotype_file: phenotypeFile, phenotype, sex, validate } = args;
+const sqlitePath = sqlite3 || 'sqlite3';
+const phenotypeFilePath = phenotypeFile || 'raw/phenotype.csv';
 
-// creates a mysql connection
-const connection = mysql.createConnection({
-    host: database.host,
-    database: database.name,
-    user: database.user,
-    password: database.password,
-    namedPlaceholders: true,
-    multipleStatements: true,
-    // debug: true,
-  }).promise();
+const inputFilePath = path.resolve(file);
+const phenotypePath = path.resolve(phenotypeFilePath);
 
 //const errorLog = getLogStream(`./failed-variants-${new Date().toISOString()}.txt`);
 const errorLog = {write: e => console.log(e)};
 const getTimestamp = timestamp({includePreviousTime: true});
+const processArgs = args => args.map(arg => ` "${arg.replace(/\n/g, ' ')}"`).join(``);
 const duration = () => {
     // shows total elapsed time, as well as elapsed time since previous step
     let [current, previous] = getTimestamp();
@@ -64,33 +58,29 @@ if (!/^(all|female|male)$/.test(sex)) {
     process.exit(1);
 }
 
-(async function main() {
-    try {
-        // let {id: phenotypeId} = await validatePhenotype(connection, phenotype);
-        let phenotypeId = phenotype;
+if (validate && !fs.existsSync(phenotypePath)) {
+    console.error(`ERROR: ${phenotypePath} does not exist.`);
+    process.exit(1);
+}
 
-        let {
-            exportVariantFilePath, 
-            exportAggregateFilePath, 
-            exportMetadataFilePath
-        } = exportVariants({
+if (!validate && !/^\d+$/.test(phenotype)) {
+    console.error(`ERROR: Association names must be validated. Please use the --validate flag.`);
+    process.exit(1);
+}
+
+
+(function main() {
+    try {
+        let phenotypeId = validate
+            ? validatePhenotype(phenotypePath, phenotype).id
+            : phenotype;
+
+        exportVariants({
             sqlitePath,
             inputFilePath,
             phenotypeId,
             sex,
         });
-
-        if (shouldImport) {
-            await importVariants({
-                connection,
-                exportVariantFilePath, 
-                exportAggregateFilePath, 
-                exportMetadataFilePath,
-                phenotypeId,
-                sex,
-                reset,
-            });
-        }
 
         process.exit(0);
     }
@@ -100,13 +90,18 @@ if (!/^(all|female|male)$/.test(sex)) {
     }
 })();
 
-// validates a phenotype by name or id
-async function validatePhenotype(connection, phenotype) {
+// validates a phenotype by name or id and returns both if found
+function validatePhenotype(phenotypePath, phenotype) {
+    // if a numeric phenotype was provided, assume we're looking up by id
+    // otherwise, look up phenotype by association name
     const phenotypeKey = /^\d+$/.test(phenotype) ? 'id' : 'name';
-    const phenotypes = await getRecords(connection, 'phenotype', {
-        [phenotypeKey]: phenotype
-    });
 
+    // read the phenotypes file and attempt to find the specified phenotype
+    const phenotypes = parseCsv(    
+        fs.readFileSync(phenotypePath),
+        {columns: ['id', 'parent_id', 'display_name', 'name', 'description', 'type']}
+    ).filter(p => p[phenotypeKey] == phenotype);
+    
     if (phenotypes.length === 0) {
         throw(`Phenotype does not exist`);
     }
@@ -125,10 +120,10 @@ function exportVariants({
     sex,
 }) {
     const inputDirectory = path.dirname(inputFilePath);
-    const databaseFilePath = path.resolve(inputDirectory, `export.${phenotypeId}.db`);
-    const exportVariantFilePath = path.resolve(inputDirectory, `export-variant.${phenotypeId}.csv`);
-    const exportAggregateFilePath = path.resolve(inputDirectory, `export-aggregate.${phenotypeId}.csv`);
-    const exportMetadataFilePath = path.resolve(inputDirectory, `export-metadata.${phenotypeId}.csv`);
+    const databaseFilePath = path.resolve(inputDirectory, `${phenotypeId}.${sex}.db`);
+    const exportVariantFilePath = path.resolve(inputDirectory, `${phenotypeId}.${sex}.variant.csv`);
+    const exportAggregateFilePath = path.resolve(inputDirectory, `${phenotypeId}.${sex}.aggregate.csv`);
+    const exportMetadataFilePath = path.resolve(inputDirectory, `${phenotypeId}.${sex}.metadata.csv`);
     const idPrefix = [null, 'all', 'female', 'male'].indexOf(sex) 
         + phenotypeId.toString().padStart(5, '0');
 
@@ -234,13 +229,17 @@ function exportVariants({
     // load data into prestaging table
     console.log(`.mode csv .import ${inputFilePath} prestage`);
     console.log(`[${duration()} s] Loading data into stage table...`);
-    const importStatus = spawnSync(`sqlite3`, [
+    const importStatus = execSync(sqlitePath + processArgs([
         databaseFilePath,
         `.mode csv`,
         `.import '${inputFilePath}' prestage`
-    ]);
+    ]));
+
     // show full import status if needed
-    // console.log(importStatus, importStatus.stdout.toString(), importStatus.stderr.toString());
+    console.log("importStatus", 
+        importStatus, 
+        importStatus.stdout ? importStatus.stdout.toString() : "Success", 
+        importStatus.stderr ? importStatus.stderr.toString() : "Success");
     
     console.log(`[${duration()} s] Beginning transaction...`);
     db.exec(`BEGIN TRANSACTION`);
@@ -311,7 +310,7 @@ function exportVariants({
     console.log(`[${duration()} s] Determining show_qq_plot flag for ${numPoints} points...`);
     db.prepare(`
         WITH ids as (
-            SELECT ${count} - ROUND(${count} * (1 - POW(cast(rowid as double) / ${numPoints} - 1, 2)))
+            SELECT ${count} - ROUND(${count} * (1 - POW(CAST(rowid as double) / ${numPoints} - 1, 2)))
             FROM stage WHERE rowid <= ${numPoints}
         ) UPDATE stage SET
             show_qq_plot = 1
@@ -331,81 +330,89 @@ function exportVariants({
     db.close();
     
     console.log(`[${duration()} s] Finished setting up stage table, exporting variants to ${exportVariantFilePath}...`);
-    const exportVariantStatus = spawnSync(sqlitePath, [
+    const exportVariantStatus = execSync(sqlitePath + processArgs([
         databaseFilePath,
         `.mode csv`,
         `.headers on`,
         `.output '${exportVariantFilePath}'`,
-        `SELECT
-            ${idPrefix} || ROW_NUMBER () OVER ( 
+        `SELECT 
+            ${idPrefix} || ROW_NUMBER () OVER (
                 ORDER BY cr.rowid, s.p_value
-            ) as id,
-            ${phenotypeId} as phenotype_id,
-            "${sex}" as sex,
-            s.chromosome,
-            s.position,
-            s.snp,
-            s.allele_reference,
-            s.allele_alternate,
-            s.p_value,
-            s.p_value_nlog,
-            s.p_value_nlog_expected,
-            s.p_value_r,
-            s.odds_ratio,
-            s.odds_ratio_r,
-            s.n,
-            s.q,
-            s.i,
-            s.show_qq_plot
-        FROM stage s
-        JOIN chromosome_range cr ON s.chromosome = cr.chromosome
+            ) as id, 
+            ${phenotypeId} as phenotype_id, 
+            '${sex}' as sex, 
+            s.chromosome, 
+            s.position, 
+            s.snp, 
+            s.allele_reference, 
+            s.allele_alternate, 
+            s.p_value, s.p_value_nlog, 
+            s.p_value_nlog_expected, 
+            s.p_value_r, 
+            s.odds_ratio, 
+            s.odds_ratio_r, 
+            s.n, s.q, 
+            s.i, 
+            s.show_qq_plot 
+        FROM stage s 
+        JOIN chromosome_range cr ON s.chromosome = cr.chromosome 
         ORDER BY cr.rowid, s.p_value`
-    ]);
+    ]));
+
+    console.log("exportVariantStatus", exportVariantStatus, 
+        exportVariantStatus.stdout ? exportVariantStatus.stdout.toString() : "Success", 
+        exportVariantStatus.stderr ? exportVariantStatus.stderr.toString() : "Success");
     
     console.log(`[${duration()} s] Exporting aggregated variants to ${exportAggregateFilePath}...`);
-    const exportAggregateStatus = spawnSync(sqlitePath, [
+    const exportAggregateStatus = execSync(sqlitePath + processArgs([
         databaseFilePath,
         `.mode csv`,
         `.headers on`,
         `.output '${exportAggregateFilePath}'`,
-        `SELECT DISTINCT
-            "${idPrefix}" || ROW_NUMBER () OVER ( 
+        `SELECT 
+            DISTINCT ${idPrefix} || ROW_NUMBER () OVER (
                 ORDER BY cr.rowid, s.p_value_nlog_aggregate
-            ) as id,    
-            ${phenotypeId} as phenotype_id,
-            "${sex}" as sex,
-            s.chromosome,
-            s.position_abs_aggregate as position_abs,
-            s.p_value_nlog_aggregate as p_value_nlog
-        FROM stage s
-        JOIN chromosome_range cr ON s.chromosome = cr.chromosome
+            ) as id, 
+            ${phenotypeId} as phenotype_id, 
+            '${sex}' as sex, 
+            s.chromosome, 
+            s.position_abs_aggregate as position_abs, 
+            s.p_value_nlog_aggregate as p_value_nlog 
+        FROM stage s 
+        JOIN chromosome_range cr ON s.chromosome = cr.chromosome 
         ORDER BY cr.rowid, p_value_nlog`
-    ]);
+    ]));
+    console.log("exportAggregateStatus", exportAggregateStatus, 
+        exportAggregateStatus.stdout ? exportAggregateStatus.stdout.toString() : "Success", 
+        exportAggregateStatus.stderr ? exportAggregateStatus.stderr.toString() : "Success");
     
     console.log(`[${duration()} s] Exporting variant metadata to ${exportMetadataFilePath}...`);
-    const exportMetadataStatus = spawnSync(sqlitePath, [
+    const exportMetadataStatus = execSync(sqlitePath + processArgs([
         databaseFilePath,
         `.mode csv`,
         `.output '${exportMetadataFilePath}'`,
         `.headers on`,
         `SELECT 
-            ${phenotypeId} as phenotype_id,
-            "${sex}" as sex, 
-            "all" as chromosome, 
+            ${phenotypeId} as phenotype_id, 
+            '${sex}' as sex, 
+            'all' as chromosome, 
             ${lambdaGC} as lambda_gc, 
             ${count} as count`,
         `.headers off`,
-        `SELECT DISTINCT
-            ${phenotypeId} as phenotype_id,
-            "${sex}" as sex,
-            s.chromosome as chromosome,
-            null as lambda_gc,
-            count(*) as count
-        FROM stage s
-        JOIN chromosome_range cr ON s.chromosome = cr.chromosome
-        GROUP BY s.chromosome
-        ORDER BY cr.rowid`,
-    ]);
+        `SELECT 
+            DISTINCT ${phenotypeId} as phenotype_id, 
+            '${sex}' as sex, 
+            s.chromosome as chromosome, 
+            null as lambda_gc, 
+            count(*) as count 
+        FROM stage s 
+        JOIN chromosome_range cr ON s.chromosome = cr.chromosome 
+        GROUP BY s.chromosome 
+        ORDER BY cr.rowid`
+    ]));
+    console.log("exportMetadataStatus", exportMetadataStatus, 
+        exportMetadataStatus.stdout ? exportMetadataStatus.stdout.toString() : "Success", 
+        exportAggregateStatus.stderr ? exportAggregateStatus.stderr.toString() : "Success");
     
     console.log([
         `[${duration()} s] Finished exporting, generated the following files:`,
@@ -421,154 +428,3 @@ function exportVariants({
     };
 }
 
-
-async function importVariants({
-    connection,
-    exportVariantFilePath, 
-    exportAggregateFilePath, 
-    exportMetadataFilePath,
-    phenotypeId,
-    sex,
-    reset,
-}) {
-
-    const partition = `\`${phenotypeId}\``; // quote partition identifier
-    const subpartition = `\`${phenotypeId}_${sex}\``; // quote subpartition identifier
-    const variantTable = `phenotype_variant`;
-    const aggregateTable = `phenotype_aggregate`;
-
-    // determine if we need to reset/create partitions
-    const [partitionRows] = await connection.execute(
-        `SELECT * FROM INFORMATION_SCHEMA.PARTITIONS
-        WHERE TABLE_NAME IN ('phenotype_variant', 'phenotype_aggregate')
-        AND PARTITION_NAME = :phenotypeId`,
-        {phenotypeId}
-    );
-
-    // There should be 6 subpartitions (3 per table)
-    if (reset || partitionRows.length !== 6) {
-        // clear variants if needed
-        for (let table of [variantTable, aggregateTable]) {
-            if (partitionRows.find(p => p.PARTITION_NAME == phenotypeId)) {
-                console.log(`[${duration()} s] Dropping partition(${partition}) on ${table}...`);
-                await connection.query(`ALTER TABLE ${table} DROP PARTITION ${partition};`)
-            }
-        }
-
-        for (let table of [variantTable, aggregateTable]) {
-            console.log(`[${duration()} s] Creating partition(${partition}) on ${table}...`);
-            await connection.query(`
-                ALTER TABLE ${table} ADD PARTITION (PARTITION ${partition} VALUES IN (${phenotypeId}) (
-                    subpartition \`${phenotypeId}_all\`,
-                    subpartition \`${phenotypeId}_female\`,
-                    subpartition \`${phenotypeId}_male\`
-                ));
-            `);
-        }
-    }
-
-    await connection.query(`
-        START TRANSACTION;
-        -- SET UNIQUE_CHECKS = 0;
-        SET AUTOCOMMIT = 0;
-    `);
-
-    console.log(`[${duration()} s] Loading variants into table...`);
-    await connection.query({
-        infileStreamFactory: path => fs.createReadStream(exportVariantFilePath),
-        sql: `LOAD DATA LOCAL INFILE "${exportVariantFilePath}"
-            INTO TABLE ${variantTable} partition (${subpartition})
-            FIELDS TERMINATED BY ','
-            IGNORE 1 LINES
-            (
-                id,
-                phenotype_id,
-                sex,
-                chromosome,
-                position,
-                snp,
-                allele_reference,
-                allele_alternate,
-                p_value,
-                p_value_nlog,
-                p_value_nlog_expected,
-                p_value_r,
-                odds_ratio,
-                odds_ratio_r,
-                n,
-                q,
-                i,
-                show_qq_plot
-            )`
-    });
-
-    console.log(`[${duration()} s] Loading aggregate variants into table...`);
-    await connection.query({
-        infileStreamFactory: path => fs.createReadStream(exportAggregateFilePath),
-        sql: `LOAD DATA LOCAL INFILE "${exportAggregateFilePath}"
-            INTO TABLE ${aggregateTable} partition (${subpartition})
-            FIELDS TERMINATED BY ','
-            IGNORE 1 LINES
-            (
-                id,
-                phenotype_id,
-                sex,
-                chromosome,
-                position_abs,
-                p_value_nlog
-            )`
-    });
-
-    console.log(`[${duration()} s] Loading metadata into table...`);
-    await connection.query(`CREATE TEMPORARY TABLE metadata_stage LIKE phenotype_metadata`);
-    await connection.query({
-        infileStreamFactory: path => fs.createReadStream(exportMetadataFilePath),
-        sql: `LOAD DATA LOCAL INFILE "${exportMetadataFilePath}"
-            INTO TABLE metadata_stage
-            FIELDS TERMINATED BY ','
-            IGNORE 1 LINES
-            (
-                phenotype_id, 
-                sex, 
-                chromosome, 
-                lambda_gc, 
-                count
-            )`
-    });
-
-    // upsert metadata
-    await connection.query(`
-        INSERT INTO phenotype_metadata 
-            (phenotype_id, sex, chromosome, lambda_gc, count)
-        SELECT 
-            phenotype_id, sex, chromosome, lambda_gc, count 
-        FROM 
-            metadata_stage
-        ON DUPLICATE KEY UPDATE
-            lambda_gc = VALUES(lambda_gc),
-            count = VALUES(count);
-    `);
-
-    // log imported variants
-    console.log(`[${duration()} s] Storing import log...`);
-    connection.execute(`
-        UPDATE phenotype SET
-            import_count = (
-                SELECT count from phenotype_metadata
-                WHERE
-                    phenotype_id = :phenotypeId AND
-                    sex = :sex AND
-                    chromosome = :chromosome
-            ),
-            import_date = NOW()
-        WHERE
-            id = :phenotypeId`,
-        {phenotypeId, sex, chromosome: 'all'}
-    );
-
-    await connection.query(`COMMIT`);
-    await connection.end();
-
-    console.log(`[${duration()} s] Done importing`);
-    return 0;
-}
