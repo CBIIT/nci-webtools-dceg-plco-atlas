422541212a0045295bb490b98bb61bc0ff35abbc
kvnjng <kvvnjng@gmail.com>
2020-05-12 16:57:16 -0400

    use new export variant script

--- database/import/scripts/import-melanoma.sh
+++ database/import/scripts/import-melanoma.sh
@@ -4,6 +4,6 @@ module use ~/mymodules
 module load nodejs
 module load SQLite_3.31.1
 sqlite3 --version
-node parallel-export-variant-hpc.js --file "raw/melanoma.csv" --phenotype 10002 --sex "all"
-node parallel-export-variant-hpc.js --file "raw/renal_cell_carcinoma.csv" --phenotype 10002 --sex "female"
-node parallel-export-variant-hpc.js --file "raw/ewings_sarcoma.csv" --phenotype 10002 --sex "male"
+node parallel-export-variant.js --file "raw/melanoma.csv" --phenotype 10002 --sex "all"
+node parallel-export-variant.js --file "raw/melanoma.csv" --phenotype 10002 --sex "female"
+node parallel-export-variant.js --file "raw/melanoma.csv" --phenotype 10002 --sex "male"
