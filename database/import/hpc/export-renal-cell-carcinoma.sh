55db315c62de0daf5e4106cd7b57e4d34f909ff9
kvnjng <kvvnjng@gmail.com>
2020-05-13 14:09:18 -0400

    move hpc scripts to hpc folder

--- database/import/hpc/export-renal-cell-carcinoma.sh
+++ database/import/hpc/export-renal-cell-carcinoma.sh
@@ -4,6 +4,6 @@ module use ~/mymodules
 module load nodejs
 module load SQLite_3.31.1
 sqlite3 --version
-node parallel-export-variant.js --file "raw/renal_cell_carcinoma.csv" --phenotype 10003 --sex "all"
-node parallel-export-variant.js --file "raw/renal_cell_carcinoma.csv" --phenotype 10003 --sex "female"
-node parallel-export-variant.js --file "raw/renal_cell_carcinoma.csv" --phenotype 10003 --sex "male"
+node parallel-export-variant.js --file "../raw/renal_cell_carcinoma.csv" --phenotype 10003 --sex "all"
+node parallel-export-variant.js --file "../raw/renal_cell_carcinoma.csv" --phenotype 10003 --sex "female"
+node parallel-export-variant.js --file "../raw/renal_cell_carcinoma.csv" --phenotype 10003 --sex "male"
