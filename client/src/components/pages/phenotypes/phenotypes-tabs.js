3ce1c5c2829015eda22f986e3510a97939f712c8
Brian Park <park-brian@users.noreply.github.com>
2020-05-26 13:35:50 -0400

    add no data placeholder to first frequency chart

--- client/src/components/pages/phenotypes/phenotypes-tabs.js
+++ client/src/components/pages/phenotypes/phenotypes-tabs.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Tab, Tabs, Form } from 'react-bootstrap';
 import paginationFactory from 'react-bootstrap-table2-paginator';
@@ -58,19 +58,24 @@ export function PhenotypesTabs(props) {
     }
   };
 
+  useEffect(() => {
+    if (!selectedPlot || (selectedPlot && selectedPlot === 'frequency')) return;
+    setSelectedPlot(selectedPlot);
+  }, [sharedState]);
+
   const titleCase = str => str.split(/[_\s]+/g)
     .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
     .join(' ');
 
-  const FrequencyTable = ({phenotypeData}) => {
+  const FrequencyTable = ({ phenotypeData }) => {
     const data = phenotypeData.categories.map((category, i) => ({
-      id: category, 
+      id: category,
       value: phenotypeData.frequency[i]
     }))
 
     const columns = [
-      {dataField: 'id', text: phenotypeData.displayName, sort: true}, 
-      {dataField: 'value', text: 'Frequency', sort: true, formatter: e => e.toLocaleString()}
+      { dataField: 'id', text: phenotypeData.displayName, sort: true },
+      { dataField: 'value', text: 'Frequency', sort: true, formatter: e => e.toLocaleString() }
     ];
 
     return <Table
@@ -84,31 +89,31 @@ export function PhenotypesTabs(props) {
         sizePerPageRenderer: paginationSizeSelector,
         pageButtonRenderer: paginationButton
       })}
-    />    
+    />
   }
 
-  const DistributionTable = ({phenotypeData, distributionKey, distributionCategory}) => {
+  const DistributionTable = ({ phenotypeData, distributionKey, distributionCategory }) => {
     console.log(phenotypeData, distributionKey, distributionCategory);
-    const {counts, percentage} = phenotypeData[distributionKey];
+    const { counts, percentage } = phenotypeData[distributionKey];
     let columns = [];
     let data = [];
 
     if (phenotypeData.type === 'categorical' || phenotypeData.type === 'continuous') {
       columns = [
-        {dataField: 'value', text: phenotypeData.displayName, sort: true}, // participant value, mapped to data categories
-        {dataField: 'type', text: distributionCategory, sort: true},  // distribution category (age, sex, ancestry)
-        {dataField: 'counts', text: 'Counts', sort: true, formatter: e => e.toLocaleString()}, // participant count
-        {dataField: 'percentage', text: 'Percentage', sort: true, formatter: e => e + '%'}, // participant percentages
+        { dataField: 'value', text: phenotypeData.displayName, sort: true }, // participant value, mapped to data categories
+        { dataField: 'type', text: distributionCategory, sort: true },  // distribution category (age, sex, ancestry)
+        { dataField: 'counts', text: 'Counts', sort: true, formatter: e => e.toLocaleString() }, // participant count
+        { dataField: 'percentage', text: 'Percentage', sort: true, formatter: e => e + '%' }, // participant percentages
       ]
-      
+
       let values = phenotypeData.distributionCategories || phenotypeData.categories;
       for (let value of values) {
         let categories = phenotypeData.categoryTypes[distributionKey]
         for (let type of categories) {
           let index = categories.indexOf(type);
           data.push({
-            value, 
-            type, 
+            value,
+            type,
             counts: counts[value][index],
             percentage: percentage[value][index]
           })
@@ -117,14 +122,14 @@ export function PhenotypesTabs(props) {
     } else {
       columns = [
         // {dataField: 'value', text: phenotypeData.displayName, sort: true}, // participant value, mapped to data categories
-        {dataField: 'type', text: distributionCategory, sort: true},  // distribution category (age, sex, ancestry)
-        {dataField: 'counts', text: 'Counts', sort: true, formatter: e => e.toLocaleString()}, // participant count
-        {dataField: 'percentage', text: 'Percentage', sort: true, formatter: e => e + '%'}, // participant percentages
+        { dataField: 'type', text: distributionCategory, sort: true },  // distribution category (age, sex, ancestry)
+        { dataField: 'counts', text: 'Counts', sort: true, formatter: e => e.toLocaleString() }, // participant count
+        { dataField: 'percentage', text: 'Percentage', sort: true, formatter: e => e + '%' }, // participant percentages
       ];
 
       data = Object.keys(counts).map(type => ({
-        type, 
-        counts: counts[type], 
+        type,
+        counts: counts[type],
         percentage: percentage[type]
       }));
     }
@@ -145,13 +150,16 @@ export function PhenotypesTabs(props) {
     </div>;
   }
 
-  useEffect(() => {
-    if (!selectedPlot || (selectedPlot && selectedPlot === 'frequency')) return ;
-    setSelectedPlot(selectedPlot);
-  }, [sharedState]);
+  const NoDataPlaceholder = ({ visible }) => <>
+    <div style={{ display: visible ? 'none' : 'block' }}>
+      <p className="h4 text-center text-secondary my-5">
+        No data exists for the selected plot
+      </p>
+    </div>
+  </>
 
   return (
-    <div style={{ minHeight: '600px'}}>
+    <div style={{ minHeight: '600px' }}>
       <LoadingOverlay active={loading} />
       <Tabs
         transition={false}
@@ -163,81 +171,81 @@ export function PhenotypesTabs(props) {
           eventKey="frequency"
           title="Frequency (All)"
           className="p-4 bg-white tab-pane-bordered rounded-0"
-          style={{ minHeight: '600px'}}>
+          style={{ minHeight: '600px' }}>
 
-          <div className="m-2 text-right">
-            <ButtonGroup 
-              size="sm" 
-              options={[
-                {label: 'Plot', value: 'plot'},
-                {label: 'Table', value: 'table'},
-              ]}
-              value={displayType.frequency}
-              onChange={value => setDisplayType({
-                ...displayType, 
-                frequency: value
-              })}
-            />              
-          </div>
+          {phenotypeData && phenotypeData.frequency && phenotypeData.categories && phenotypeData.frequency.length ? <>
+            <div className="m-2 text-right">
+              <ButtonGroup
+                size="sm"
+                options={[
+                  { label: 'Plot', value: 'plot' },
+                  { label: 'Table', value: 'table' },
+                ]}
+                value={displayType.frequency}
+                onChange={value => setDisplayType({
+                  ...displayType,
+                  frequency: value
+                })}
+              />
+            </div>
 
-          {phenotypeData && phenotypeData.frequency && phenotypeData.categories && <>
 
             {displayType.frequency === 'plot' && <div className="text-center">
               {phenotypeData.type !== 'continuous' && <PieChart
-                    data={phenotypeData.frequency}
-                    categories={phenotypeData.categories} />}
+                data={phenotypeData.frequency}
+                categories={phenotypeData.categories} />}
 
               {phenotypeData.type === 'continuous' && <AreaChart
-                    data={phenotypeData.frequency}
-                    categories={phenotypeData.categories}
-                    xTitle={phenotypeData.displayName}
-                    yTitle="Number of Participants" />}
+                data={phenotypeData.frequency}
+                categories={phenotypeData.categories}
+                xTitle={phenotypeData.displayName}
+                yTitle="Number of Participants" />}
             </div>}
 
-            {displayType.frequency === 'table' && 
+            {displayType.frequency === 'table' &&
               <FrequencyTable phenotypeData={phenotypeData} />}
-          </>}
+          </> : <NoDataPlaceholder visible={loading} />}
         </Tab>
 
         {[
-          {key: 'frequencyByAge', title: 'Frequency By Age'},
-          {key: 'frequencyBySex', title: 'Frequency By Sex'},
-          {key: 'frequencyByAncestry', title: 'Frequency By Ancestry'},
+          { key: 'frequencyByAge', title: 'Frequency By Age' },
+          { key: 'frequencyBySex', title: 'Frequency By Sex' },
+          { key: 'frequencyByAncestry', title: 'Frequency By Ancestry' },
         ].map(t =>
           <Tab
             key={t.key}
             eventKey={t.key}
             title={t.title}
             className="p-4 bg-white tab-pane-bordered rounded-0"
-            style={{ minHeight: '600px'}}>
+            style={{ minHeight: '600px' }}>
 
             {/* <pre>{JSON.stringify(phenotypeData, null, 2)}</pre> */}
 
-            {!loading && phenotypeData[t.key] && phenotypeData[t.key].counts && Object.keys(phenotypeData[t.key].counts).length ? <> 
+            {!loading && phenotypeData[t.key] && phenotypeData[t.key].counts && Object.keys(phenotypeData[t.key].counts).length ? <>
               <div className="d-flex align-items-center justify-content-between">
-                <ButtonGroup 
-                  size="sm" 
+                <ButtonGroup
+                  size="sm"
                   disabled={displayType[t.key] === 'table'}
                   options={[
-                    {label: 'Counts', value: 'counts'},
-                    {label: 'Percentage', value: 'percentage'},
+                    { label: 'Counts', value: 'counts' },
+                    { label: 'Percentage', value: 'percentage' },
                   ]}
                   value={frequencyType[t.key]}
                   onChange={value => setFrequencyType({
-                    ...frequencyType, 
+                    ...frequencyType,
                     [t.key]: value
                   })}
-                />                     
+                />
 
-                <ButtonGroup 
-                  size="sm" 
+                <ButtonGroup
+                  size="sm"
                   options={[
-                    {label: 'Plot', value: 'plot'},
-                    {label: 'Table', value: 'table'},
+                    { label: 'Plot', value: 'plot' },
+                    { label: 'Table', value: 'table' },
                   ]}
                   value={displayType[t.key]}
                   onChange={value => setDisplayType({
-                    ...displayType, 
+                    ...displayType,
                     [t.key]: value
                   })}
                 />
@@ -246,31 +254,26 @@ export function PhenotypesTabs(props) {
               {phenotypeData && phenotypeData[t.key] && <>
                 {displayType[t.key] === 'plot' && <div className="text-center">
                   {((/continuous/.test(phenotypeData.type) || (/binary/.test(phenotypeData.type) && t.key === 'frequencyByAge')) ? GroupedAreaChart : BarChart)({
-                      data: phenotypeData[t.key][frequencyType[t.key]],
-                      categoryPrefix: t.key.match(/frequencyBy(Age|Sex|Ancestry)/)[1],
-                      type: phenotypeData.type,
-                      categories: (phenotypeData.type === 'binary')
-                        ? phenotypeData.distributionCategories
-                        : phenotypeData.categoryTypes[t.key],
-                      xTitle: phenotypeData.displayName,
-                      yTitle: frequencyType[t.key] === 'counts' ? 'Number of Participants' : '% of Participants',
-                      fill: true,
-                      formatPercent: frequencyType[t.key] === 'percentage',
-                    })}
+                    data: phenotypeData[t.key][frequencyType[t.key]],
+                    categoryPrefix: t.key.match(/frequencyBy(Age|Sex|Ancestry)/)[1],
+                    type: phenotypeData.type,
+                    categories: (phenotypeData.type === 'binary')
+                      ? phenotypeData.distributionCategories
+                      : phenotypeData.categoryTypes[t.key],
+                    xTitle: phenotypeData.displayName,
+                    yTitle: frequencyType[t.key] === 'counts' ? 'Number of Participants' : '% of Participants',
+                    fill: true,
+                    formatPercent: frequencyType[t.key] === 'percentage',
+                  })}
                 </div>}
 
-                {displayType[t.key] === 'table' && 
+                {displayType[t.key] === 'table' &&
                   <DistributionTable
                     phenotypeData={phenotypeData}
                     distributionKey={t.key}
                     distributionCategory={t.title.split(' ').pop()} />}
               </>}
-            </> : 
-            <div style={{ display: loading ? 'none' : 'block' }}>
-              <p className="h4 text-center text-secondary my-5">
-                No data exists for the selected plot
-              </p>
-            </div>}
+            </> : <NoDataPlaceholder visible={loading} />}
           </Tab>
         )}
 
@@ -279,17 +282,17 @@ export function PhenotypesTabs(props) {
           title="Related Phenotypes"
           className="p-4 bg-white tab-pane-bordered rounded-0"
           style={{ minHeight: '50vh' }}>
-          {!loading && selectedPlot === 'related-phenotypes' && phenotypeData && phenotypeData.relatedPhenotypes && 
-            <PhenotypesRelated 
-                  relatedData={phenotypeData.relatedPhenotypes}
-                  onClick={e => {
-                    if (props.onSubmit) {
-                      let id = e.points[0].customdata.phenotype_id;
-                      let phenotype = phenotypes.flat.find(p => p.id == id);
-                      props.onSubmit(phenotype);
-                    }
-                  }}
-          />}
+          {!loading && selectedPlot === 'related-phenotypes' && phenotypeData && phenotypeData.relatedPhenotypes &&
+            <PhenotypesRelated
+              relatedData={phenotypeData.relatedPhenotypes}
+              onClick={e => {
+                if (props.onSubmit) {
+                  let id = e.points[0].customdata.phenotype_id;
+                  let phenotype = phenotypes.flat.find(p => p.id == id);
+                  props.onSubmit(phenotype);
+                }
+              }}
+            />}
         </Tab>
       </Tabs>
 
