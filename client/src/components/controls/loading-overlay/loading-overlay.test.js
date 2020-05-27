78092f1e5844a92189ea747f854ff77b20718845
Brian Park <park-brian@users.noreply.github.com>
2020-05-27 11:27:10 -0400

    add test module for LoadingOverlay component

---
+++ client/src/components/controls/loading-overlay/loading-overlay.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { LoadingOverlay } from './loading-overlay';
+
+describe('LoadingOverlay Module', function () {
+    test('LoadingOverlay renders correctly', () => {
+        const props = {active: true, content: 'Test'};
+        render(<LoadingOverlay {...props} />)
+
+        // expect loading overlay to be visible
+        expect(screen.getByTestId('LoadingOverlay')).toBeVisible();
+
+        // expect loader to contain supplied text
+        expect(screen.getByTestId('DefaultLoader')).toHaveTextContent(props.content);
+    });
+});
