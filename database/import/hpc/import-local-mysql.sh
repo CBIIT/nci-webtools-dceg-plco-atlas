a97db608331d93d4eeb8853f31ad0700bebed3a9
Kevin  Jiang <jiangk3@biowulf.nih.gov>
2020-06-02 15:47:17 -0400

    swap new mysql config before starting server

--- database/import/hpc/import-local-mysql.sh
+++ database/import/hpc/import-local-mysql.sh
@@ -4,19 +4,36 @@ DB_USER=$1
 DB_PASS=$2
 ARCHIVE_FILE=$3
 
-echo "LOADING MYSQL MODULE..."
-module load mysql/8.0
+echo "LOADING MODULES (MySQL-8.0, GNU-Parallel)..."
+module load mysql/8.0 parallel
+echo
 
 echo "RESTORING MYSQL DATABASE FROM ARCHIVE FILE $ARCHIVE_FILE ..."
-local_mysql restore --archivefile=$ARCHIVE_FILE
+time local_mysql restore --archivefile=$ARCHIVE_FILE
+echo
+
+echo "INJECTING SLURM_JOB_ID ENV VAR TO MYSQL CONFIGURATION FILE..."
+envsubst < mysql.config > my.cnf
+
+echo "COPYING OVER NEW MYSQL CONFIGURATION FILE..."
+rm /lscratch/$SLURM_JOB_ID/mysql/my.cnf
+cp ./my.cnf /lscratch/$SLURM_JOB_ID/mysql/
+echo
 
 echo "STARTING MYSQL SERVER..."
 local_mysql start
+echo
 
-echo "LOGGING INTO MYSQL SHELL WITH DB_USER=$DB_USER, DB_PASS=$DB_PASS, HOST=$SLURM_NODELIST, PORT=55555..."
-mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="show tables;"
+echo "LOGGING INTO MYSQL WITH DB_USER=$DB_USER, DB_PASS=$DB_PASS, HOST=$SLURM_NODELIST, PORT=55555..."
+time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="SELECT @@local_infile; SELECT @@innodb_buffer_pool_size; SELECT @@innodb_read_io_threads; SELECT @@innodb_write_io_threads; SELECT @@innodb_log_buffer_size; SELECT @@innodb_log_file_size; SELECT @@innodb_flush_log_at_trx_commit;"
+echo
 
-echo "EXITED MYSQL SHELL..."
+#echo "STOPPING MYSQL SERVER..."
+#local_mysql stop
+#echo
 
-echo "STOPPING MYSQL SERVER..."
-local_mysql stop
+#echo "SAVING MYSQL SERVER..."
+#time local_mysql archive --archivefile=/data/$USER/plco/mysql/mysql-archive-$SLURM_JOB_ID.tgz
+#echo
+
+echo "DONE"
