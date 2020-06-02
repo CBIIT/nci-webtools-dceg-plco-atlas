046ca6bb4c27e20d9b24ab827996098fc6d8d6bd
kvnjng <kvvnjng@gmail.com>
2020-06-02 19:04:31 -0400

    test gnu parallel import on local mysql'

--- database/import/hpc/import-local-mysql.sh
+++ database/import/hpc/import-local-mysql.sh
@@ -28,15 +28,20 @@ echo "LOGGING INTO MYSQL WITH DB_USER=$DB_USER, DB_PASS=$DB_PASS, HOST=$SLURM_NO
 time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="SELECT @@local_infile; SELECT @@innodb_buffer_pool_size; SELECT @@innodb_read_io_threads; SELECT @@innodb_write_io_threads; SELECT @@innodb_log_buffer_size; SELECT @@innodb_log_file_size; SELECT @@innodb_flush_log_at_trx_commit; SELECT @@key_buffer_size;"
 echo
 
-# echo "SPAWNING TEST PARALLEL IMPORT PROCESSES..."
-# time parallel -j 3 < gnu-parallel-import-melanoma-test.txt
-# echo
+echo "SPAWNING TEST PARALLEL IMPORT PROCESSES..."
+time parallel -j 3 < gnu-parallel-import-melanoma-test.txt
+echo
 
 echo "STOPPING MYSQL SERVER..."
 local_mysql stop
 echo
 
-echo "SAVING MYSQL SERVER..."
+echo "COPYING MYSQL SERVER TO /data/$USER/plco/mysql/mysql-instance-$SLURM_JOB_ID ..."
+mkdir /data/$USER/plco/mysql/mysql-instance-$SLURM_JOB_ID
+time cp -avr /lscratch/$SLURM_JOB_ID/mysql /data/$USER/plco/mysql/mysql-instance-$SLURM_JOB_ID
+echo
+
+echo "ARCHIVING MYSQL SERVER..."
 time local_mysql archive --archivefile=/data/$USER/plco/mysql/mysql-archive-$SLURM_JOB_ID.tgz
 echo
 
