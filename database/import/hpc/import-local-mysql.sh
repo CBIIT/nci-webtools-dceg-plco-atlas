e032703be2e75c4e8f4d510838fe8bf3d65e632d
Kevin  Jiang <jiangk3@biowulf.nih.gov>
2020-06-01 21:03:45 -0400

    fix mysql login from script

--- database/import/hpc/import-local-mysql.sh
+++ database/import/hpc/import-local-mysql.sh
@@ -13,11 +13,8 @@ local_mysql restore --archivefile=$ARCHIVE_FILE
 echo "STARTING MYSQL SERVER..."
 local_mysql start
 
-echo "LOGGING INTO MYSQL SHELL..."
-mysql -u $DB_USER -p"DB_PASS" --host=$SLURM_NODELIST --port=55555 plcogwas
-
-show tables
-\q
+echo "LOGGING INTO MYSQL SHELL WITH DB_USER=$DB_USER, DB_PASS=$DB_PASS, HOST=$SLURM_NODELIST, PORT=55555..."
+mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="show tables;"
 
 echo "EXITED MYSQL SHELL..."
 
