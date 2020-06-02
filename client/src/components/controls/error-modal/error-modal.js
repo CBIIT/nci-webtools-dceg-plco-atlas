ba8cf864f801ff0de0673cc47e6a5b4623537d83
Brian Park <park-brian@users.noreply.github.com>
2020-06-02 18:16:54 -0400

    create error modal

---
+++ client/src/components/controls/error-modal/error-modal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Modal, Button } from 'react-bootstrap';
+import { updateError } from '../../../services/actions';
+
+export function ErrorModal(props) {
+    const error = useSelector(store => store.error);
+    const dispatch = useDispatch();
+    const closeErrorModal = () => dispatch(updateError({visible: false}));
+
+    return (
+        <Modal show={error.visible} onHide={closeErrorModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>Error</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <p dangerouslySetInnerHTML={{__html: error.message}} />
+            </Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary" onClick={closeErrorModal}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
