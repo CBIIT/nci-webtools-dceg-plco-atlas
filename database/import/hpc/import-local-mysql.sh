37cfadb2a5aebf73c651314aadfc787866dd6e41
Kevin  Jiang <jiangk3@biowulf.nih.gov>
2020-06-04 17:16:46 -0400

    export TMPDIR to lscratch space

--- database/import/hpc/import-local-mysql.sh
+++ database/import/hpc/import-local-mysql.sh
@@ -3,6 +3,7 @@
 export DB_USER=$1
 export DB_PASS=$2
 export ARCHIVE_FILE=$3
+export TMPDIR=/lscratch/$SLURM_JOB_ID
 
 echo "LOADING MODULES (MySQL-8.0, GNU-Parallel, NodeJS)..."
 module load mysql/8.0 parallel nodejs
@@ -30,11 +31,11 @@ time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas -
 echo
 
 echo "INJECTING CREDENTIALS TO gnu-parallel-import-melanoma-test.txt ..."
-envsubst < gnu-parallel-import-melanoma-test-9.txt > gnu-parallel-import-melanoma-test-envsubst.txt
+envsubst < gnu-parallel-import-melanoma-test-3x-full.txt  > gnu-parallel-import-melanoma-test-envsubst.txt
 echo
 
 echo "SPAWNING TEST PARALLEL IMPORT PROCESSES..."
-time parallel -j 9 < gnu-parallel-import-melanoma-test-envsubst.txt
+time parallel -j 3 < gnu-parallel-import-melanoma-test-envsubst.txt
 echo
 
 echo "STOPPING MYSQL SERVER..."
