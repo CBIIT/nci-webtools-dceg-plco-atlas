632cee37d1d05f391173b81bc258c46bf60ee268
kvnjng <kvvnjng@gmail.com>
2020-06-15 17:36:54 -0400

    use largemem partition for db import

--- database/import/hpc/run-import-local-mysql.sh
+++ database/import/hpc/run-import-local-mysql.sh
@@ -55,4 +55,4 @@ if [ -z "$5" ]
 fi
 NUM_JOBS=$5
 
-sbatch --gres=lscratch:200 --mem=256g --cpus-per-task=72 --partition=norm --time=08:00:00 --wrap="sh import-local-mysql.sh $DB_USER $DB_PASS $ARCHIVE_FILE $COMMANDS_FILE $NUM_JOBS"
+sbatch --gres=lscratch:200 --mem=512g --cpus-per-task=36 --partition=largemem --time=08:00:00 --wrap="sh import-local-mysql.sh $DB_USER $DB_PASS $ARCHIVE_FILE $COMMANDS_FILE $NUM_JOBS"
