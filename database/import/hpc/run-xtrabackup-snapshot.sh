10816d7df2127fc06207f425a8892ca8a84adaf9
kvnjng <kvvnjng@gmail.com>
2020-06-19 11:19:37 -0400

    backup to local first then use aws cli to upload to s3

--- database/import/hpc/run-xtrabackup-snapshot.sh
+++ database/import/hpc/run-xtrabackup-snapshot.sh
@@ -5,8 +5,8 @@
 if [ -z "$1" ]
     then
         echo "ERROR: No MySQL db username supplied..."
-        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
-        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup aws_access_key_id aws_secret_access_key"
+        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <BUCKET_NAME> <BUCKET_FOLDER>"
+        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup bucket_name bucket_folder"
         exit 1
 fi
 DB_USER=$1
@@ -16,8 +16,8 @@ DB_USER=$1
 if [ -z "$2" ]
     then
         echo "ERROR: No MySQL db password supplied..."
-        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
-        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup aws_access_key_id aws_secret_access_key"
+        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <BUCKET_NAME> <BUCKET_FOLDER>"
+        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup bucket_name bucket_folder"
         exit 1
 fi
 DB_PASS=$2
@@ -27,8 +27,8 @@ DB_PASS=$2
 if [ -z "$3" ]
     then
         echo "ERROR: No MySQL base directory path supplied..."
-        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
-        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup aws_access_key_id aws_secret_access_key"
+        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <BUCKET_NAME> <BUCKET_FOLDER>"
+        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup bucket_name bucket_folder"
         exit 1
 fi
 BASE_DIR=$3
@@ -38,32 +38,32 @@ BASE_DIR=$3
 if [ -z "$4" ]
     then
         echo "ERROR: No xtrabackup target directory path supplied..."
-        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
-        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup aws_access_key_id aws_secret_access_key"
+        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <BUCKET_NAME> <BUCKET_FOLDER>"
+        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup bucket_name bucket_folder"
         exit 1
 fi
 TARGET_DIR=$4
 
-# ARGUMENT 5: Input AWS ACCESS KEY
-# AWS_ACCESS_KEY_ID = "aws_access_key_id"
+# ARGUMENT 5: Input AWS S3 bucket name
+# BUCKET_NAME = "bucket_name"
 if [ -z "$5" ]
     then
         echo "ERROR: No AWS access key supplied..."
-        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
-        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup aws_access_key_id aws_secret_access_key"
+        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <BUCKET_NAME> <BUCKET_FOLDER>"
+        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup bucket_name bucket_folder"
         exit 1
 fi
-AWS_ACCESS_KEY_ID=$5
+BUCKET_NAME=$5
 
-# ARGUMENT 6: Input AWS SECRET ACCESS KEY
-# AWS_SECRET_ACCESS_KEY = "aws_secret_access_key"
+# ARGUMENT 6: Input AWS S3 bucket folder
+# BUCKET_FOLDER = "bucket_folder"
 if [ -z "$6" ]
     then
         echo "ERROR: No AWS secret access key supplied..."
-        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <AWS_ACCESS_KEY_ID> <AWS_SECRET_ACCESS_KEY>"
-        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup aws_access_key_id aws_secret_access_key"
+        echo "USAGE: sh run-xtrabackup-snapshot.sh <DB_USER> <DB_PASS> <BASE_DIR> <TARGET_DIR> <BUCKET_NAME> <BUCKET_FOLDER>"
+        echo "EXAMPLE: sh run-xtrabackup-snapshot.sh sample_username sample_password /data/jiangk3/plco/mysql/saved-instance-path /data/jiangk3/plco/mysql/rds-backup bucket_name bucket_folder"
         exit 1
 fi
-AWS_SECRET_ACCESS_KEY=$6
+BUCKET_FOLDER=$6
 
-sbatch --gres=lscratch:200 --mem=64g --cpus-per-task=32 --partition=norm --time=08:00:00 --wrap="sh xtrabackup-snapshot.sh $DB_USER $DB_PASS $BASE_DIR $TARGET_DIR $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY"
+sbatch --gres=lscratch:200 --mem=64g --cpus-per-task=32 --partition=norm --time=08:00:00 --wrap="sh xtrabackup-snapshot.sh $DB_USER $DB_PASS $BASE_DIR $TARGET_DIR $BUCKET_NAME $BUCKET_FOLDER"
