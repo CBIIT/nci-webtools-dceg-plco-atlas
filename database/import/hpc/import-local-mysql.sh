1d8674dece1698f4956984dda99c0969ffed5906
kvnjng <kvvnjng@gmail.com>
2020-07-20 18:17:30 -0400

    generate scripts for gnu parallel import

--- database/import/hpc/import-local-mysql.sh
+++ database/import/hpc/import-local-mysql.sh
@@ -3,7 +3,7 @@
 export DB_USER=$1
 export DB_PASS=$2
 export BASE_DIR=$3
-export COMMANDS_FILE=$4
+export DATA_DIR=$4
 export NUM_JOBS=$5
 export TMPDIR=/lscratch/$SLURM_JOB_ID
 
@@ -20,12 +20,14 @@ echo "DROPPING EXISTING STAGE TABLES"
 time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="SELECT @@local_infile; SELECT @@innodb_buffer_pool_instances; SELECT @@innodb_buffer_pool_size; SELECT @@innodb_read_io_threads; SELECT @@innodb_write_io_threads; SELECT @@innodb_log_buffer_size; SELECT @@innodb_log_file_size; SELECT @@innodb_flush_log_at_trx_commit; SELECT @@innodb_file_per_table; SELECT @@key_buffer_size; SET GLOBAL unique_checks = 0; SET foreign_key_checks = 0; SET sql_log_bin = 0; SELECT @@unique_checks; SELECT @@foreign_key_checks; SELECT @@sql_log_bin; DROP TABLE participant_data_stage; DROP TABLE participant_data_category_stage;"
 echo
 
+sh create-gnu-parallel-script.sh $DATA_DIR
+
 echo "INJECTING CREDENTIALS TO gnu-parallel-import-melanoma-test.txt ..."
-envsubst < $COMMANDS_FILE > gnu-parallel-import-commands.txt
+envsubst < gnu-parallel-import-all-phenotypes.txt > gnu-parallel-import-commands.txt
 echo
 
 echo "SPAWNING TEST PARALLEL IMPORT PROCESSES..."
-time parallel -j$NUM_JOBS < gnu-parallel-import-commands.txt
+time parallel --jobs $NUM_JOBS < gnu-parallel-import-commands.txt
 echo
 
 echo "STOPPING MYSQL SERVER..."
--- database/import/hpc/run-import-local-mysql.sh
+++ database/import/hpc/run-import-local-mysql.sh
@@ -5,7 +5,7 @@
 if [ -z "$1" ]
     then
         echo "ERROR: No MySQL db username supplied..."
-        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <COMMANDS_FILE> <NUM_JOBS>"
+        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <DATA_DIR> <NUM_JOBS>"
         echo "EXAMPLE: sh run-import-local-mysql.sh sample_username sample_password /data/jiangk3/plco/mysql/mysql-instance-base gnu-parallel-import-melanoma-test-9x-full 9"
         exit 1
 fi
@@ -16,7 +16,7 @@ DB_USER=$1
 if [ -z "$2" ]
     then
         echo "ERROR: No MySQL db password supplied..."
-        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <COMMANDS_FILE> <NUM_JOBS>"
+        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <DATA_DIR> <NUM_JOBS>"
         echo "EXAMPLE: sh run-import-local-mysql.sh sample_username sample_password /data/jiangk3/plco/mysql/mysql-instance-base gnu-parallel-import-melanoma-test-9x-full 9"
         exit 1
 fi
@@ -27,32 +27,32 @@ DB_PASS=$2
 if [ -z "$3" ]
     then
         echo "ERROR: No gnu-parallel import commands file supplied..."
-        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <COMMANDS_FILE> <NUM_JOBS>"
+        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <DATA_DIR> <NUM_JOBS>"
         echo "EXAMPLE: sh run-import-local-mysql.sh sample_username sample_password /data/jiangk3/plco/mysql/mysql-instance-base gnu-parallel-import-melanoma-test-9x-full 9"
         exit 1
 fi
 BASE_DIR=$3
 
-# ARGUMENT 4: Input gnu-parallel import commands file
-# COMMANDS_FILE = "gnu-parallel-import-melanoma-test-9x-full"
+# ARGUMENT 4: Data directory path
+# DATA_DIR="../raw/output"
 if [ -z "$4" ]
     then
         echo "ERROR: No gnu-parallel import commands file supplied..."
-        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <COMMANDS_FILE> <NUM_JOBS>"
+        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <DATA_DIR> <NUM_JOBS>"
         echo "EXAMPLE: sh run-import-local-mysql.sh sample_username sample_password /data/jiangk3/plco/mysql/mysql-instance-base gnu-parallel-import-melanoma-test-9x-full 9"
         exit 1
 fi
-COMMANDS_FILE=$4
+DATA_DIR=$4
 
 # ARGUMENT 5: Input number of parallel import jobs to run
 # NUM_JOBS = 9
 if [ -z "$5" ]
     then
         echo "ERROR: No number of parallel import jobs to run supplied..."
-        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <COMMANDS_FILE> <NUM_JOBS>"
+        echo "USAGE: sh run-import-local-mysql.sh <DB_USER> <DB_PASS> <BASE_DIR> <DATA_DIR> <NUM_JOBS>"
         echo "EXAMPLE: sh run-import-local-mysql.sh sample_username sample_password /data/jiangk3/plco/mysql/mysql-instance-base gnu-parallel-import-melanoma-test-9x-full 9"
         exit 1
 fi
 NUM_JOBS=$5
 
-sbatch --gres=lscratch:200 --mem=512g --cpus-per-task=36 --partition=largemem --time=08:00:00 --wrap="sh import-local-mysql.sh $DB_USER $DB_PASS $BASE_DIR $COMMANDS_FILE $NUM_JOBS"
+sbatch --gres=lscratch:200 --mem=512g --cpus-per-task=72 --partition=largemem --time=08:00:00 --wrap="sh import-local-mysql.sh $DB_USER $DB_PASS $BASE_DIR $DATA_DIR $NUM_JOBS"
