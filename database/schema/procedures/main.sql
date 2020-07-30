8f9384de161bb4773377fa221e93b961f642b596
Brian Park <park-brian@users.noreply.github.com>
2020-07-30 15:53:50 -0400

    add procedure to migrate phenotype points

--- database/schema/procedures/main.sql
+++ database/schema/procedures/main.sql
@@ -1,5 +1,4 @@
 DELIMITER $$
-
 DROP PROCEDURE IF EXISTS execute_sql $$
 CREATE PROCEDURE execute_sql(IN sql_text varchar(16000))
 BEGIN
@@ -27,7 +26,7 @@ END IF;
 END $$
 
 DROP PROCEDURE IF EXISTS insert_participant_data $$
-create procedure insert_participant_data()
+CREATE PROCEDURE insert_participant_data()
   BEGIN
   DECLARE done INT;
   DECLARE name VARCHAR(200);
@@ -60,6 +59,50 @@ create procedure insert_participant_data()
   CLOSE phenotype_cursor;
 END $$
 
+DROP PROCEDURE IF EXISTS update_phenotype_point $$
+CREATE PROCEDURE update_phenotype_point()
+  BEGIN
+  DECLARE done INT;
+  DECLARE phenotype_id INT;
+  DECLARE sex VARCHAR(200);
+  DECLARE partitions_exist INT;
+
+  DECLARE phenotype_cursor CURSOR FOR
+    select distinct p.id, pm.sex as sex from phenotype p
+    join phenotype_metadata pm on pm.phenotype_id = p.id
+    where pm.count > 0 and pm.chromosome = 'all';
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+  OPEN phenotype_cursor;
+  SET done = 0;
+  REPEAT
+    FETCH phenotype_cursor INTO phenotype_id, sex;
+
+    -- create partition for points if they do not already exist
+    SELECT COUNT(*) INTO partitions_exist FROM INFORMATION_SCHEMA.PARTITIONS
+    WHERE TABLE_NAME = "phenotype_point" AND PARTITION_NAME = phenotype_id;
+
+    IF partitions_exist = 0 THEN
+        call execute_sql(CONCAT('
+            ALTER TABLE phenotype_point ADD PARTITION (PARTITION `', phenotype_id, '` VALUES IN (', phenotype_id, ') (
+                subpartition `', phenotype_id, '_all`,
+                subpartition `', phenotype_id, '_female`,
+                subpartition `', phenotype_id, '_male`
+            ))'
+        ));
+    END IF;
+
+    -- insert variants into partition
+    call execute_sql(CONCAT('
+        INSERT INTO phenotype_point PARTITION (`', phenotype_id, '_', sex, '`)
+        (phenotype_id, sex, chromosome, position, snp, p_value_nlog, p_value_nlog_expected)
+        SELECT ', phenotype_id, ', "', sex, '", chromosome, position, snp, p_value_nlog, p_value_nlog_expected
+        FROM phenotype_variant PARTITION (`', phenotype_id, '_', sex, '`)
+        WHERE show_qq_plot = 1 OR p_value_nlog > 3'
+    ));
+  UNTIL done END REPEAT;
+  CLOSE phenotype_cursor;
+END $$
 
 
 DELIMITER ;
