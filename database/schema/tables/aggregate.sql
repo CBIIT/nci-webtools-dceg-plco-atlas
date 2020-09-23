256a8b12460c3c189d940694941b8e47d57d5da5
Brian Park <park-brian@users.noreply.github.com>
2020-09-23 19:04:27 -0400

    add schema for portable aggregate table

---
+++ database/schema/tables/aggregate.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS ${table_name};
+CREATE TABLE IF NOT EXISTS ${table_name} (
+    `id` BIGINT AUTO_INCREMENT NOT NULL,
+    `phenotype_id` INTEGER NOT NULL,
+    `sex` VARCHAR(20),
+    `ancestry` VARCHAR(20),
+    `chromosome` ENUM(
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+        '21',
+        '22',
+        'X',
+        'Y'
+    ) NOT NULL,
+    `position_abs` BIGINT NOT NULL,
+    `p_value_nlog` DOUBLE NOT NULL,
+    PRIMARY KEY (id, phenotype_id)
+) PARTITION BY list(phenotype_id) (PARTITION `0` VALUES IN (0));
