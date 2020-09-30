9d6b9c5f2cdfea5b8fadb5393123dcd1d6dc6e9e
Brian Park <park-brian@users.noreply.github.com>
2020-09-30 12:08:08 -0400

    show additional columns for variant tables

--- client/src/components/pages/gwas/summary/summary-table.js
+++ client/src/components/pages/gwas/summary/summary-table.js
@@ -79,13 +79,19 @@ export function SummaryResultsTable() {
     },
     {
       dataField: 'allele_reference',
-      text: 'Ref. Allele',
-      headerTitle: _ => 'Reference Allele',
+      text: 'Eff. Allele',
+      headerTitle: _ => 'Effect Allele [Frequency]',
+      formatter: (cell, row) => {
+        return `${cell} [${row.allele_frequency}]`
+      }
     },
     {
       dataField: 'allele_alternate',
-      text: 'Alt. Allele',
-      headerTitle: _ => 'Alternate Allele',
+      text: 'Non-Eff. Allele',
+      headerTitle: _ => 'Non-Effect Allele [Frequency]',
+      formatter: (cell, row) => {
+        return `${cell} [${(1 - row.allele_frequency).toPrecision(4)}]`;
+      }
     },
     selectedPhenotype && selectedPhenotype.type === 'continuous' && {
       dataField: 'beta',
@@ -110,7 +116,18 @@ export function SummaryResultsTable() {
       dataField: 'p_value',
       text: 'P-Value',
       sort: true
-    }
+    },
+    {
+      dataField: 'p_value_heterogenous',
+      text: 'P-Value Het.',
+      headerTitle: _ => 'P-Value Heterogenous',
+    },
+    {
+      dataField: 'n',
+      text: 'N',
+      headerTitle: _ => 'Sample Size',
+    },
+
   ].filter(Boolean);
 
   const updateSummaryTableData = (key, params) => {
