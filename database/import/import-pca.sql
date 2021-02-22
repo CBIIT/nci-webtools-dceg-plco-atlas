882ba78f61c1b9a5e56845c7f59e9583d84323e8
Brian Park <park-brian@users.noreply.github.com>
2021-02-22 16:38:36 -0500

    remove alias

--- database/import/import-pca.sql
+++ database/import/import-pca.sql
@@ -7,7 +7,10 @@ START TRANSACTION;
 
 SET autocommit = 0;
 
-CREATE TABLE `pca_stage` (
+truncate table principal_component_analysis;
+
+drop table if exists `pca_stage`;
+CREATE TABLE if not exists `pca_stage`  (
     `plco_id` varchar(200),
     `pc1` double,
     `pc2` double,
@@ -35,120 +38,120 @@ LOAD DATA LOCAL INFILE "../raw/pca.tsv" INTO TABLE pca_stage
     FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"'
     IGNORE 1 ROWS;
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 1, ps.pc1
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 2, ps.pc2
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 3, ps.pc3
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 4, ps.pc4
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 5, ps.pc5
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 6, ps.pc6
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 7, ps.pc7
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 8, ps.pc8
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 9, ps.pc9
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 10, ps.pc10
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 11, ps.pc11
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 12, ps.pc12
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 13, ps.pc13
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 14, ps.pc14
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 15, ps.pc15
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 16, ps.pc16
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 17, ps.pc17
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 18, ps.pc18
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 19, ps.pc19
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
 
 
-INSERT INTO principal_component_analysis pca (participant_id, principal_component, value)
+INSERT INTO principal_component_analysis (participant_id, principal_component, value)
 SELECT p.id, 20, ps.pc20
 FROM pca_stage ps
 JOIN participant p on ps.plco_id = p.plco_id;
