a244cfeb2f149715edfc976b57d8641d5c409805
kvnjng <kvvnjng@gmail.com>
2021-03-29 14:06:50 -0600

    set foreign key checks to 0 before importing SQL files

--- database/import/hpc/create-local-mysql-base.sh
+++ database/import/hpc/create-local-mysql-base.sh
@@ -34,28 +34,55 @@ time local_mysql --basedir $BASE_DIR start
 echo
 
 echo "LOGGING INTO MYSQL AS ROOT AND CREATING $DB_USER USER, CREATING PLCOGWAS DATABASE..."
-time mysql -u root -p$DB_PASS --socket=$BASE_DIR/mysql.sock --execute="CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS'; GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost' WITH GRANT OPTION; CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION; CREATE DATABASE plcogwas CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+time mysql -u root -p$DB_PASS --socket=$BASE_DIR/mysql.sock --execute="
+    CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS'; 
+    GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost' WITH GRANT OPTION; 
+    CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+    GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION; 
+    CREATE DATABASE plcogwas CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+"
 echo
 
 echo "LOGGING INTO MYSQL WITH DB_USER=$DB_USER, DB_PASS=$DB_PASS, HOST=$SLURM_NODELIST, PORT=55555, SETTING MYSQL CONFIGURATIONS..."
-time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="SELECT @@local_infile; SELECT @@innodb_buffer_pool_size; SELECT @@innodb_read_io_threads; SELECT @@innodb_write_io_threads; SELECT @@innodb_log_buffer_size; SELECT @@innodb_log_file_size; SELECT @@innodb_flush_log_at_trx_commit; SELECT @@innodb_file_per_table; SELECT @@key_buffer_size; SET GLOBAL unique_checks = 0; SET unique_checks = 0; SELECT @@unique_checks;"
+time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="
+    SELECT @@local_infile; 
+    SELECT @@innodb_buffer_pool_size; 
+    SELECT @@innodb_read_io_threads; 
+    SELECT @@innodb_write_io_threads; 
+    SELECT @@innodb_log_buffer_size; 
+    SELECT @@innodb_log_file_size; 
+    SELECT @@innodb_flush_log_at_trx_commit; 
+    SELECT @@innodb_file_per_table; 
+    SELECT @@key_buffer_size; 
+    SET GLOBAL unique_checks = 0; 
+    SET unique_checks = 0; 
+    SELECT @@unique_checks;
+"
 echo
 
 echo "IMPORTING TABLE SCHEMA, INDEXES, AND PROCEDURES..."
-time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 plcogwas --execute="source ../../schema/tables/main.sql; source ../../schema/indexes/main.sql; source ../../schema/procedures/main.sql;"
+time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas --execute="
+    set FOREIGN_KEY_CHECKS=0; 
+    source ../../schema/tables/main.sql; 
+    source ../../schema/indexes/main.sql; 
+    source ../../schema/procedures/main.sql;
+    source ../import-lookup-tables.sql;
+    source ../import-chromosome-range.sql;
+    source ../import-gene.sql;
+"
 echo
 
-echo "IMPORTING LOOKUP TABLES..."
-time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < ../import-lookup-tables.sql
-echo
+# echo "IMPORTING LOOKUP TABLES..."
+# time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < ../import-lookup-tables.sql
+# echo
 
-echo "IMPORTING CHROMOSOME RANGES..."
-time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < ../import-chromosome-range.sql
-echo
+# echo "IMPORTING CHROMOSOME RANGES..."
+# time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < ../import-chromosome-range.sql
+# echo
 
-echo "IMPORTING GENES..."
-time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < ../import-gene.sql
-echo
+# echo "IMPORTING GENES..."
+# time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < ../import-gene.sql
+# echo
 
 echo "IMPORTING PHENOTYPE MAPPINGS..."
 time node ../import-phenotype.js --file ../raw/phenotype.csv --host $SLURM_NODELIST --port 55555 --db_name plcogwas --user $DB_USER --password $DB_PASS
