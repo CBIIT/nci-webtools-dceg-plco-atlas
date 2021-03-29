2b00487ec3b955a51ed08defa807c26b73a6b1b5
kvnjng <kvvnjng@gmail.com>
2021-03-29 12:20:21 -0600

    import lambda gc ld score values during master mysql creation in HPC

--- database/import/hpc/create-local-mysql-base.sh
+++ database/import/hpc/create-local-mysql-base.sh
@@ -77,6 +77,10 @@ echo "IMPORTING PARTICIPANT COUNTS..."
 time node ../update-participant-count.js --host $SLURM_NODELIST --port 55555 --db_name plcogwas --user $DB_USER --password $DB_PASS
 echo
 
+echo "IMPORTING LAMBDA GC LD SCORES..."
+time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < < ../import-lambda-gc-ld-score.sql
+echo
+
 echo "IMPORTING PC DATA..."
 time mysql -u $DB_USER -p$DB_PASS --host=$SLURM_NODELIST --port=55555 --local-infile=1 plcogwas < ../import-pca.sql
 echo
