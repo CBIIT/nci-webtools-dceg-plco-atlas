df64157aa15974452e3bae7dad9ea78e5ef4de52
Brian Park <park-brian@users.noreply.github.com>
2021-06-22 14:49:13 -0400

    add credits

--- client/src/components/pages/home/home.js
+++ client/src/components/pages/home/home.js
@@ -163,8 +163,13 @@ export function Home({ links }) {
       </div>
       <div className="bg-egg py-4">
         <div className="container my-3 text-dark">
-          Credits: TBD
-          <br />
+          <h2 className="h4">Credits</h2>
+
+          <p>
+            Mitchell Machiela, Neal Freedman, Wen-Yi Huang, Wendy Wong, Sonja Berndt, Mustapha Abubakar, Jonas De Almeida, Jada Hislop, Erikka Loftfield, Jennifer Loukissas, Joshua Sampson, Montse Garcia-Closas, Stephen Chanock and colleagues at the <a href="https://dceg.cancer.gov/" target="_blank">Division of Cancer Epidemiology and Genetics (DCEG)</a>, <a href="https://www.cancer.gov/" target="_blank">National Cancer Institute (NCI)</a>, <a href="https://www.nih.gov/" target="_blank">National Institutes of Health (NIH)</a>; Eric Miller, Paul Pinsky, Neeraj Saxena, Claire Zhu, Lori Minasian and colleagues at the <a href="https://prevention.cancer.gov/" target="_blank">Division of Cancer Prevention, NCI, NIH</a>; Michael Furr, Jerome Mabie, Shannon Merkle, Craig Williams, Patrick Wright, Thomas Riley and staff at <a href="https://www.imsweb.com/" target="_blank">Information Management Services, Inc.</a>; Bari Ballew, Casey Dagnall, Jia Liu, Kristine Jones, Cameron Palmer, Aurelie Vogt, Meredith Yeager, Bin Zhu, Amy Hutchinson, Belynda Hicks and staff at the <a href="https://dceg.cancer.gov/about/organization/cgr" target="_blank">Cancer Genomics Research Laboratory, DCEG, NCI</a>, <a href="https://frederick.cancer.gov/" target="_blank">Frederick National Laboratory for Cancer Research (FNLCR)</a>, <a href="https://www.leidos.com/company/subsidiaries/leidos-biomedical-research" target="_blank">Leidos Biomedical Research, Inc.</a>; Norma Diaz-Mayoral and staff at the <a href="https://ncifrederick.cancer.gov/Programs/Science/Csp/Lab.aspx?Id=27" target="_blank">BioProcessing and Trial Logistics Laboratory, FNLCR, Leidos Biomedical Research, Inc.</a>; Mary Ferrell, Erik Neidinger and staff at the NCI at <a href="https://frederick.cancer.gov/science/nci-frederick-central-repository" target="_blank">Frederick Central Repository</a>, <a href="https://www.atcc.org/">American Type Culture Collection</a>; Kevin Jiang, Brian Park, Kai-Ling Chen and staff at <a href="https://www.essential-soft.com/" target="_blank">Essential Software Inc.</a>, <a href="https://datascience.cancer.gov/" target="_blank">Center for Biomedical Informatics and Information Technology, NCI.</a>
+
+          </p>
+
           {/* Citation: TBD
           <br /> */}
           GWAS Explorer's <a href="https://github.com/CBIIT/nci-webtools-dceg-plco-atlas" target="_blank" alt="Link to open GitHub">source code</a> is available under the <a href="./assets/license.txt" target="_blank" alt="Link to MIT license">MIT license</a>, an <a href="https://opensource.org" target="_blank" alt="Link to open source initiative">Open Source Initiative</a> approved license.
