f890a19dd536f71a48852f3cf4ee82759951d8d4
Judy Yao <xiaozheng.yao@nih.gov>
2023-10-03 10:47:23 -0400

    add link to display name

--- client/src/components/pages/gwas/summary/summary-search-criteria.js
+++ client/src/components/pages/gwas/summary/summary-search-criteria.js
@@ -5,92 +5,117 @@ import { asTitleCase } from './utils';
 import { query } from '../../../../services/query';
 
 export const SummaryResultsSearchCriteria = () => {
-  const {
-    selectedPhenotypes,
-    selectedStratifications,
-    selectedChromosome,
-    selectedPlot,
-    isPairwise,
-    manhattanPlotView,
-    nlogpMin,
-    nlogpMax,
-    bpMin,
-    bpMax,
-    submitted,
-    shareID,
-  } = useSelector(state => state.summaryResults);
+	const {
+		selectedPhenotypes,
+		selectedStratifications,
+		selectedChromosome,
+		selectedPlot,
+		isPairwise,
+		manhattanPlotView,
+		nlogpMin,
+		nlogpMax,
+		bpMin,
+		bpMax,
+		submitted,
+		shareID,
+	} = useSelector((state) => state.summaryResults);
 
-  const [phenotypes, setPhenotypes] = useState([]);
+	const [phenotypes, setPhenotypes] = useState([]);
 
-  useEffect(() => {
-    Promise.all(
-      submitted && selectedPhenotypes && selectedStratifications
-        ? (isPairwise ? [0, 1] : [0]).map(async index => ({
-            ...(selectedPhenotypes[index] || selectedPhenotypes[0]),
-            stratification: [
-              selectedStratifications[index].ancestry,
-              selectedStratifications[index].sex
-            ]
-              .filter(str => str !== 'all')
-              .map(asTitleCase)
-              .join(' '),
-            resultsCounts: (await query('metadata', {
-              phenotype_id: (selectedPhenotypes[index] || selectedPhenotypes[0])
-                .id,
-              ancestry: selectedStratifications[index].ancestry,
-              sex: selectedStratifications[index].sex,
-              chromosome: 'all'
-            }))[0]
-          }))
-        : []
-    ).then(setPhenotypes);
-  }, [submitted, selectedPhenotypes, selectedStratifications, isPairwise]);
+	useEffect(() => {
+		Promise.all(
+			submitted && selectedPhenotypes && selectedStratifications
+				? (isPairwise ? [0, 1] : [0]).map(async (index) => ({
+						...(selectedPhenotypes[index] || selectedPhenotypes[0]),
+						stratification: [
+							selectedStratifications[index].ancestry,
+							selectedStratifications[index].sex,
+						]
+							.filter((str) => str !== 'all')
+							.map(asTitleCase)
+							.join(' '),
+						resultsCounts: (
+							await query('metadata', {
+								phenotype_id: (
+									selectedPhenotypes[index] || selectedPhenotypes[0]
+								).id,
+								ancestry: selectedStratifications[index].ancestry,
+								sex: selectedStratifications[index].sex,
+								chromosome: 'all',
+							})
+						)[0],
+				  }))
+				: []
+		).then(setPhenotypes);
+	}, [submitted, selectedPhenotypes, selectedStratifications, isPairwise]);
 
-  return (
-    <div className="p-3 mb-2 bg-white border rounded-0 d-flex justify-content-between">
-      {!(submitted && selectedPhenotypes && selectedStratifications) ? (
-        <strong className="d-flex align-items-center text-muted">
-          No phenotype(s) selected.
-        </strong>
-      ) : (
-        <div>
-          {phenotypes.map((p, i) => (
-            <div className={isPairwise && +i === 0 ? 'mb-2' : 'mb-0'}>
-              <strong>
-                {p.display_name} ({p.stratification}) -{' '}
-                <small>{p.resultsCounts.count.toLocaleString()} variants</small>
-                {p.type === 'binary'
-                ? <>
-                  <small className="ml-2">{(p.resultsCounts.participant_count_case || 0).toLocaleString()} cases</small>                  
-                  <small className="ml-2">{(p.resultsCounts.participant_count_control || 0).toLocaleString()} controls</small>                  
-                </>
-                : <>
-                    <small className="ml-2">{(p.resultsCounts.participant_count_case || p.resultsCounts.participant_count || 0).toLocaleString()} participants</small>
-                </>}
-                
-              </strong>
-              <div className="small muted">{p.description}</div>
-            </div>
-          ))}
-        </div>
-      )}
-      <ShareLink
-        disabled={!submitted}
-        shareID={shareID}
-        params={{
-          selectedPhenotypes,
-          selectedStratifications,
-          selectedChromosome,
-          selectedPlot,
-          isPairwise,
-          manhattanPlotView,
-          nlogpMin,
-          nlogpMax,
-          bpMin,
-          bpMax,
-          submitted,
-        }}
-      />
-    </div>
-  );
+	return (
+		<div className='p-3 mb-2 bg-white border rounded-0 d-flex justify-content-between'>
+			{!(submitted && selectedPhenotypes && selectedStratifications) ? (
+				<strong className='d-flex align-items-center text-muted'>
+					No phenotype(s) selected.
+				</strong>
+			) : (
+				<div>
+					{phenotypes.map((p, i) => (
+						<div className={isPairwise && +i === 0 ? 'mb-2' : 'mb-0'}>
+							<strong>
+								<a href={p.link}>{p.display_name}</a> ({p.stratification}) -{' '}
+								<small>{p.resultsCounts.count.toLocaleString()} variants</small>
+								{p.display_name.includes('Machiela') ? (
+									p.type === 'binary' ? (
+										<>
+											<small className='ml-2'>
+												{(
+													p.resultsCounts.participant_count_case || 0
+												).toLocaleString()}{' '}
+												cases
+											</small>
+											<small className='ml-2'>
+												{(
+													p.resultsCounts.participant_count_control || 0
+												).toLocaleString()}{' '}
+												controls
+											</small>
+										</>
+									) : (
+										<>
+											<small className='ml-2'>
+												{(
+													p.resultsCounts.participant_count_case ||
+													p.resultsCounts.participant_count ||
+													0
+												).toLocaleString()}{' '}
+												participants
+											</small>
+										</>
+									)
+								) : (
+									''
+								)}
+							</strong>
+							<div className='small muted'>{p.description}</div>
+						</div>
+					))}
+				</div>
+			)}
+			<ShareLink
+				disabled={!submitted}
+				shareID={shareID}
+				params={{
+					selectedPhenotypes,
+					selectedStratifications,
+					selectedChromosome,
+					selectedPlot,
+					isPairwise,
+					manhattanPlotView,
+					nlogpMin,
+					nlogpMax,
+					bpMin,
+					bpMax,
+					submitted,
+				}}
+			/>
+		</div>
+	);
 };
