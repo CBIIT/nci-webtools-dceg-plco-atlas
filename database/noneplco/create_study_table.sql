07e2656fca95370a7fef4aafdea935488c7a7ab8
Judy Yao <xiaozheng.yao@nih.gov>
2023-10-06 16:07:25 -0400

    update study.sql

--- database/noneplco/create_study_table.sql
+++ database/noneplco/create_study_table.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS `study` (
     `id` INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `pubmed_id` VARCHAR(200),
     `display_name` VARCHAR(200),
-    `description` MEDIUMTEXT
+    `description` MEDIUMTEXT,
+    `link` VARCHAR(200)
 );
